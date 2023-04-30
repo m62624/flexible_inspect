7dce5db6769d8ccbb75f2033f701f152d11a22a4
Mansur <mansur62624@gmail.com>
2023-04-30 23:45:20 +0600

    feat: added function `get_extra` to get **extra values
    So we use a class as a template we can't get **extra from an instance because it's a class, so we use this hack, we take the values **extra from the `template` using `regex` expressions

--- src/pyst_errors.rs
+++ src/pyst_errors.rs
@@ -12,3 +12,30 @@ pub fn throw_error(py: Python, error_class: &PyAny) -> PyResult<PyErr> {
     let pyerr = PyErr::new::<PyException, _>(shared_obj);
     Ok(pyerr)
 }
+ use crate::*;
+ use pyo3::exceptions::PyException;
+ use pyo3::types::PyType;
+pub fn get_extra<T: AsRef<str> + Send + Sync>(
+    obj: &PyType,
+    attr: T,
+    regex: T,
+) -> PyResult<Vec<String>> {
+    let (pattern, attr_value) = (
+        Regex::new(regex.as_ref()).unwrap(),
+        obj.getattr(attr.as_ref())?.to_string(),
+    );
+    if pattern.is_match(&attr_value) {
+        Ok(pattern
+            .captures_iter(&attr_value)
+            .map(|cap| {
+                cap.get(0).map_or(String::new(), |m| {
+                    m.as_str().trim_matches('{').trim_matches('}').to_string()
+                })
+            })
+            .collect::<Vec<String>>())
+    } else {
+        return Err(exceptions::PyNameError::new_err(format!(
+            "**extra is not defined (from `template`)",
+        )));
+    }
+ }
