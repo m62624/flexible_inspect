d1299d43ce423b1ac0b941737dc93c9111f652c1
Mansur <mansur62624@gmail.com>
2023-05-01 20:11:59 +0600

    feat: added structure Validator

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-use pyo3::{prelude::*, types::PyType};
+use async_mutex::Mutex;
+use pyo3::prelude::*;
 use regex::Regex;
 use std::sync::Arc;
-
 #[derive(Debug, Clone)]
 pub struct PythonSafeObject {
-    original_class: Arc<PyType>,
+    original_class: Arc<Mutex<PyObject>>,
     regex_collection: Vec<Regex>,
+}
+
+#[derive(Debug, Clone)]
+#[pyclass]
+pub struct Validator {
+    inner: Vec<PythonSafeObject>,
 }
