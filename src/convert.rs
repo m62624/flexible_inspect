1e4217d8bbabea3fb5f5fa33c6433bc2665c37ff
Mansur <mansur62624@gmail.com>
2023-05-01 14:24:04 +0600

    feat: added functions for `pyObject` conversions

--- src/convert.rs
+++ src/convert.rs
@@ -1,20 +1,48 @@
+use pyo3::exceptions::PyTypeError;
+
 use crate::*;
-use std::collections::HashMap;
-pub fn get_hashmap_from_dict<'py>(
-    flags_with_errors: &'py PyDict,
-) -> PyResult<HashMap<&'py str, &'py PyAny>> {
-    let (mut result, dict) = (HashMap::new(), flags_with_errors);
-    let keys = dict.keys();
-    for key in keys {
-        let key_str: &str = key.extract()?;
-        let value = match dict.get_item(&key) {
-            Some(value) => value,
-            None => return Err(PyErr::new::<exceptions::PyTypeError, _>("None value`")),
-        };
-        result.insert(key_str, value);
+pub fn pylist_to_vec_string(pylist: &PyList) -> Vec<String> {
+    let mut vec_string = Vec::new();
+    for item in pylist.iter() {
+        if let Ok(s) = item.extract::<String>() {
+            vec_string.push(s);
+        }
+    }
+    vec_string
+}
+///Получаем `PyList` для конвертаций в `Iterator<PyType>` (проще работать с коллекций объектами)
+pub fn py_list_to_py_types<'py>(pylist: &'py PyList) -> PyResult<Vec<&'py PyType>> {
+    let mut result = Vec::with_capacity(pylist.len());
+    for element in pylist.iter() {
+        result.push(element.downcast::<PyType>()?);
     }
     Ok(result)
 }
+
+// !!! Примерные шаблоны будущих или же на них основанных функций конвертаций
+
+// pub fn py_any_to_py_type<'py>(py: Python, pyobj: &'py Py<PyAny>) -> PyResult<PyType> {
+//     match pyobj.downcast::<PyType>(py) {
+//         Ok(value) => Ok(value),
+//         Err(_) => Err(PyErr::new::<PyTypeError, _>("Expected a PyType object")),
+//     }
+// }
+// pub fn get_hashmap_from_dict<'py>(
+//     flags_with_errors: &'py PyDict,
+// ) -> PyResult<HashMap<&'py str, &'py PyAny>> {
+//     let (mut result, dict) = (HashMap::new(), flags_with_errors);
+//     let keys = dict.keys();
+//     for key in keys {
+//         let key_str: &str = key.extract()?;
+//         let value = match dict.get_item(&key) {
+//             Some(value) => value,
+//             None => return Err(PyErr::new::<exceptions::PyTypeError, _>("None value`")),
+//         };
+//         result.insert(key_str, value);
+//     }
+//     Ok(result)
+// }
+
 // pub fn (obj_class: &PyAny) -> PyResult<HashMap<String, &PyAny>> {
 //     let mut result = HashMap::new();
 //     for attr_name in obj_class.dir().into_iter() {
