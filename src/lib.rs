05eee93f52920edca6978fd24567cf10260fd4b7
Mansur <mansur62624@gmail.com>
2023-05-01 13:15:57 +0600

    feat: added constructor for Validator
    - Saves data to Vector<tuple>,
    - added validate(bytes) function template
    This will be the storage format for now. The `python::types` for `rust` do not implement `hash` or `eq`, so there is no way to use Hashmap with these data types

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,88 @@
+/// Модуль отвечает за конвертацию `Python Type` в `Rust Type` и обратно
 mod convert;
+/// Модуль отвеччает за получение кастомных ошибок и после проброс их же в `Python`
 mod pyst_errors;
-use pyo3::{exceptions, prelude::*, types::PyDict};
+use pyo3::{
+    exceptions::{self},
+    prelude::*,
+    types::{PyList, PyType},
+};
+/// Регулярные выражени, является сердцем валидатора (вся логика обработки это Regex)
+use regex::Regex;
+use std::str;
 
+/// Класс содержит List ошибок по которым будет проходить обработка (re-export -> Python)
 #[pyclass]
 struct Validator {
-    factory_data: PyObject,
+    factory_data: Vec<(PyObject, Vec<String>)>,
 }
-
+/// Модуль для инициализации `Validator`
 mod init_validator {
     use super::*;
     #[pymethods]
     impl Validator {
+        /// Констуктор (re-export -> Python)
         #[new]
-        fn py_new(factory_data: PyObject) -> PyResult<Validator> {
-            Python::with_gil(|py| match factory_data.downcast::<PyDict>(py) {
-                Ok(value) => Ok({
-                    convert::get_hashmap_from_dict(value)?;
-                    // BaseError::py_show_attr(py, value);
+        pub fn py_new(factory_data: PyObject) -> PyResult<Validator> {
+            // Используем привязку `GIL` для безопасной обраотки переменных из python
+            // downcast служит проверкой типа объекта Python
+            Python::with_gil(|py| match factory_data.downcast::<PyList>(py) {
+                Ok(list_error_class) => Ok({
+                    let mut factory_data: Vec<(Py<PyAny>, Vec<String>)> = Vec::new();
+                    for item in convert::py_list_to_py_types(list_error_class)? {
+                        factory_data.push((
+                            item.to_object(py),
+                            pyst_errors::info_data::get_extra(item, "template", r"\{.+?\}")?,
+                        ));
+                    }
+                    // dbg!(&factory_data);
                     Validator { factory_data }
                 }),
                 Err(_) => Err(PyErr::new::<exceptions::PyTypeError, _>(
-                    "factory_data must be a dictionary",
+                    "'factoryData' must be a dictionary",
                 )),
             })
         }
-        fn __repr__(slf: &PyCell<Self>) -> PyResult<String> {
-            Ok(format!(
-                "{}({})",
-                slf.get_type().name()?,
-                slf.borrow().factory_data
-            ))
+        pub fn validate(&self, raw_data: &[u8]) -> PyResult<()> {
+            // проверка на конветрацию из byte в String (UTF8)
+            match str::from_utf8(raw_data) {
+                Ok(text) => {
+                    // Используем привязку `GIL` для безопасной обраотки переменных из python
+                    // downcast служит проверкой типа объекта Python
+                    Python::with_gil(|py| {
+                        for (class, extra) in &self.factory_data {
+                            // convert::py_any_to_py_type(py, class);
+                            // println!("{:#?}", );
+                            // println!("{:#?} | {:#?}", class, extra);
+                        }
+                    })
+                }
+                Err(_) => {
+                    return Err(PyErr::new::<exceptions::PyTypeError, _>(
+                        "'factoryData' must be a dictionary",
+                    ))
+                }
+            };
+            Ok(())
+        }
+        /// минимальный набор методов для отладки
+        fn __repr__(&self) -> PyResult<String> {
+            Ok(format!("Validator({:#?})", self.factory_data))
         }
 
         fn __str__(&self) -> String {
-            self.factory_data.to_string()
+            format!("{:#?}", self.factory_data)
         }
     }
 }
 // Модуль export необходим для импортирования : функций, методов, классов в `Python`
 mod export {
     use super::*;
+    /// отвечает за re-export, классов, методов, функций
     #[pymodule]
     fn pystval(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         m.add_class::<Validator>()?;
-        m.add_function(wrap_pyfunction!(pyst_errors::throw_error, m)?)?;
+        // m.add_function(wrap_pyfunction!(init_validator::validate, m)?)?;
         Ok(())
     }
 }
