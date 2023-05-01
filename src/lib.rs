c6a44f3b282553aed7611cb050e7273756070874
Mansur <mansur62624@gmail.com>
2023-05-01 20:24:42 +0600

    feat: added a template constructor

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,30 @@ use regex::Regex;
 use std::sync::Arc;
 #[derive(Debug, Clone)]
 pub struct PythonSafeObject {
-    original_class: Arc<Mutex<PyObject>>,
+    original_class: PyObject,
     regex_collection: Vec<Regex>,
 }
 
 #[derive(Debug, Clone)]
 #[pyclass]
 pub struct Validator {
-    inner: Vec<PythonSafeObject>,
+    inner: Arc<Mutex<Vec<PythonSafeObject>>>,
+}
+mod init_validator {
+    use super::*;
+    #[pymethods]
+    impl Validator {
+        #[new]
+        fn py_constructor() -> Self {
+            todo!()
+        }
+        /// минимальный набор методов для отладки
+        fn __repr__(&self) -> PyResult<String> {
+            Ok(format!("Validator({:#?})", self.inner))
+        }
+        fn __str__(&self) -> String {
+            format!("{:#?}", self.inner)
+        }
+    }
+    impl Validator {}
 }
