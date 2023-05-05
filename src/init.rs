471fa92ff173593e7032ca57f26a75c5f25500f1
Mansur <mansur62624@gmail.com>
2023-05-06 00:38:45 +0600

    feat: changed structure `Validator`
    - Now we don't store a nested structure. There is now only `Validator`.
    - We have implemented the ability to check all the regulars at once, without iterating over each regular expression
    - You can now easily find errors using a regular expression instead of the other way around

--- src/init.rs
+++ src/init.rs
@@ -1,31 +1,29 @@
 use crate::*;
 use ::regex::Regex;
-use futures::try_join;
 use lazy_static::lazy_static;
 use pyo3::exceptions;
 use pyo3::exceptions::PyTypeError;
 pub mod extra {
     use super::*;
     /// Получаем **extra переменные в виде коллекций
-    pub async fn extra_from_class<'a, T: AsRef<str> + Send + Sync>(
+    pub fn extra_from_class<'a, T: AsRef<str> + Send + Sync>(
         class_template: &types::PyType,
         attr: T,
-    ) -> PyResult<Option<Box<dyn Iterator<Item = String> + 'a>>> {
+    ) -> PyResult<Option<Vec<String>>> {
         let attr_value = class_template.getattr(attr.as_ref())?.to_string();
         lazy_static! {
             static ref RE: Regex = Regex::new(r"\{.+?\}").unwrap();
         }
         if RE.is_match(&attr_value) {
-            Ok(Some(Box::new(
+            Ok(Some(
                 RE.captures_iter(&attr_value)
                     .map(|cap| {
                         cap.get(0).map_or(String::new(), |m| {
                             m.as_str().trim_matches('{').trim_matches('}').to_string()
                         })
                     })
-                    .collect::<Vec<_>>()
-                    .into_iter(),
-            )))
+                    .collect(),
+            ))
         } else {
             Ok(None)
         }
@@ -33,35 +31,42 @@ pub mod extra {
 }
 
 pub mod regex {
-
     use super::*;
-
-    pub fn check_regex<T: AsRef<str> + Send + Sync>(regex_template: T) -> PyResult<Regex> {
-        match Regex::new(regex_template.as_ref()) {
-            Ok(value) => return Ok(value),
-            Err(_) => Err(PyErr::new::<PyTypeError, _>("Invalid regular expression")),
-        }
-    }
-    pub async fn regex_from_class<'a>(
-        class_template: &'a types::PyType,
-    ) -> PyResult<Box<dyn Iterator<Item = Regex> + 'a>> {
-        match class_template
+    pub fn collection_regex(
+        hash_map: &mut HashMap<String, String>,
+        rgx_set: &mut Vec<String>,
+        error_class: &types::PyType,
+        id_from_error_class: String,
+    ) -> PyResult<()> {
+        match error_class
             .getattr(RULES_FROM_CLASS_PY)?
             .downcast::<types::PyList>()
         {
-            Ok(pylist) => Ok(Box::new(
-                pylist
-                    .iter()
-                    .map(|packed| match packed.extract::<String>() {
-                        Ok(s) => check_regex(s),
-                        Err(_) => Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                            "'{}' must be a 'String'",
-                            packed
-                        ))),
+            Ok(pylist) => Ok(pylist
+                .iter()
+                .map(|packed| {
+                    Ok(match packed.extract::<String>() {
+                        Ok(line_to_regex) => match Regex::new(&line_to_regex) {
+                            Ok(_) => {
+                                rgx_set.push(line_to_regex.to_owned());
+                                hash_map.insert(line_to_regex, id_from_error_class.to_owned());
+                            }
+                            Err(_) => {
+                                return Err(PyErr::new::<PyTypeError, _>(format!(
+                                    "{} --- Invalid regular expression",
+                                    packed
+                                )));
+                            }
+                        },
+                        Err(_) => {
+                            return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
+                                "'{}' must be a 'String'",
+                                packed
+                            )))
+                        }
                     })
-                    .collect::<Result<Vec<_>, _>>()?
-                    .into_iter(),
-            )),
+                })
+                .collect::<Result<_, _>>()?),
             Err(_) => Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
                 "'{}' must be a 'List[ string, string... ]'",
                 RULES_FROM_CLASS_PY
@@ -79,21 +84,5 @@ pub mod bytes_utf8 {
                 "'text_raw' must be UTF-8 encoded",
             )),
         }
-    }
-}
-pub mod data {
-    use super::*;
-    pub fn multi_data<'a>(
-        one_error: &'a types::PyType,
-    ) -> PyResult<(
-        Box<dyn Iterator<Item = Regex> + 'a>,
-        Option<Box<dyn Iterator<Item = String> + 'a>>,
-    )> {
-        async_std::task::block_on(async {
-            try_join!(
-                regex::regex_from_class(&one_error),
-                extra::extra_from_class(&one_error, MESSAGE_WITH_EXTRA_FROM_CLASS_PY)
-            )
-        })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,94 +2,65 @@ mod export_py;
 mod global_const;
 mod init;
 mod make_py_error;
-mod move_not_copy;
 use global_const::*;
-use init::data;
 use pyo3::prelude::*;
 use pyo3::types;
 use pyo3::ToPyObject;
 use regex::Regex;
+use regex::RegexSet;
+use std::collections::HashMap;
 use std::str;
-// Реалзиуем Clone, так как необходимо передавать `self` в `async fn`
-#[derive(Debug, Clone)]
-pub struct PythonSafeObject {
-    // Храним переданный класс на основе которого создаем ошибку
-    original_class: PyObject,
-    // Храним **extra для заполнения информаций ошибки
-    extra: Option<Vec<String>>,
-    // Храним регулярные выражения для обработки текста
-    rgxs: Vec<Regex>,
-}
-// Реалзиуем Clone, так как необходимо передавать `self` в `async fn`
-#[derive(Debug, Clone)]
+use uuid::Uuid;
+
 #[pyclass]
+#[derive(Debug, Clone)]
 pub struct Validator {
-    // Храним коллекцию, где структура обернута в `Box`,
-    // так как необходимо будет запускать несколько `task` которые будут владеть этой структурой
-    inner: Vec<PythonSafeObject>,
+    // Храним KEY: `regex` и VALUE: `uuid`
+    selection_rules: HashMap<String, String>,
+    // Храним KEY: `uuid` и VALUE: `ClassError`
+    python_classes: HashMap<String, PyObject>,
+    // Собираем все regex и *одним проходом* проверяем все регулярки
+    all_rules: RegexSet,
 }
+// Методы которые будут использоваться в `Python`
 #[pymethods]
 impl Validator {
     #[new]
-    /// Конструктор для валидатора, принимает лист классов ошибок
-    fn py_constructor(errors_list: PyObject) -> PyResult<Self> {
+    fn py_constructor(flags_errors: PyObject) -> PyResult<Self> {
+        // Храним все регулярные выражения вместе с классом ошибки
+        let mut python_classes = HashMap::new();
+        // Храним все регулярные выражения, но будем отбирать и создавать их, только,
+        // если по ним было найденно совпадение
+        let mut selection_rules = HashMap::new();
+        // Храним все правила, чтобы разом провести
+        // поиск по всем регулярным выражениям за *одни раз*
+        let mut all_rules = vec![];
         Python::with_gil(|py| {
-            Ok(Validator {
-                inner: errors_list
-                    .downcast::<types::PyList>(py)
-                    .map_err(|e| e)?
-                    .iter()
-                    .map(|boxed_class| {
-                        let obj = boxed_class.downcast::<types::PyType>()?;
-                        let data = data::multi_data(&obj);
-                        Ok(PythonSafeObject {
-                            rgxs: data?.0.collect(),
-                            extra: match data?.1 {
-                                Some(value) => Some(value.collect()),
-                                None => None,
-                            },
-                            original_class: obj.into(),
-                        })
-                    })
-                    .collect::<Result<Vec<_>, _>>()
-                    .map_err(|e: PyErr| e)?,
-            })
-        })
-    }
-
-    // Служит для запуска async метода
-    #[pyo3(name = "validate")]
-    #[pyo3(signature = (text_bytes))]
-    fn py_validate<'py>(
-        &self,
-        py: Python<'py>,
-        text_bytes: &types::PyBytes,
-    ) -> PyResult<&'py PyAny> {
-        // let slf = self.self_context(py)?;
-        let slf = self.clone();
-        let text = init::bytes_utf8::convert(text_bytes)?;
-        pyo3_asyncio::async_std::future_into_py(py, async move {
-            slf.core_validate(Box::new(text)).await?;
-            Ok(Python::with_gil(|py| py.None()))
-        })
-    }
-}
-impl Validator {
-    pub async fn core_validate<'py>(&self, text_raw: Box<String>) -> PyResult<()> {
-        let mut result: Result<(), PyErr> = Ok(());
-        for obj in self.inner.iter() {
-            for rgx in &obj.rgxs {
-                if let Some(extra) =
-                    init::regex::regex_find(&init::regex::get_regex(rgx)?, &text_raw, &obj.extra)
-                        .await
-                {
-                    result = make_py_error::throw_error(&obj.original_class, extra);
-                }
+            // Получаем каждый класс с коллекций
+            for py_class in flags_errors.downcast::<types::PyList>(py)? {
+                // Класс ошибки из`Python`
+                let py_class = py_class.downcast::<types::PyType>()?;
+                // Уникальный UUID, для каждого класса
+                let id = Uuid::new_v4().to_string();
+                init::regex::collection_regex(
+                    &mut selection_rules,
+                    &mut all_rules,
+                    &py_class,
+                    id.to_owned(),
+                )?;
+                python_classes.insert(id, py_class.to_object(py));
             }
-        }
-        result
-    }
-    pub fn self_context(&self, py: Python) -> PyResult<Validator> {
-        self.to_object(py).extract::<Validator>(py)
+            Ok::<(), PyErr>(())
+        })?;
+        let x = Self {
+            selection_rules,
+            python_classes,
+            all_rules: RegexSet::new(&all_rules).unwrap(),
+        };
+        dbg!(&x);
+        Ok(x)
     }
 }
+// методы которые являются только `Rust` методами или вспомагательными
+// для `Python` методов
+impl Validator {}
