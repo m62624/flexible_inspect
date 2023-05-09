c86d61736027be5b39f6b890059327fd314ca06f
Mansur <mansur62624@gmail.com>
2023-05-09 14:42:55 +0600

    chore: saved all files for template

--- src/validate.rs
+++ src/validate.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use pyo3::exceptions;
 
 use super::*;
@@ -16,6 +18,9 @@ impl TemplateValidator {
             }
             for (rule, rule_status) in self.all_simple_rules.iter() {
                 just_look_at_this_simple(py, self, rule, rule_status, &text)?;
+            }
+            for (rule, rule_status) in self.all_simple_rules.iter() {
+                just_look_at_this_hard(py, self, rule, rule_status, &text)?;
             }
             Ok(())
         })
@@ -39,6 +44,53 @@ fn just_look_at_this_simple(
         flag = true;
         for name in &extra_names {
             match capture.name(&name) {
+                Some(value) => {
+                    extra_values.insert(name.to_string(), value.as_str().to_string());
+                }
+                None => {
+                    extra_values.insert(name.to_string(), "___".to_string());
+                }
+            }
+        }
+    }
+    // Если указан `MustBeFoundhere`, но случайно указан `{}` в template, заполняем заглушкой
+    /*
+            class NoKeyFound:
+            template = "Не найден ключ"
+            rules = {r"key=\d+?": It.MustBeFoundHere}
+    */
+    // Если необходимо получить данные от ошибки должно быть `NotToBeFoundHere`, после
+    // указываем название группы для получения результата
+    if extra_values.is_empty() {
+        for blank in extra_names {
+            extra_values.insert(blank, format!(
+                " \n| Do not use `{{ ... }}` along with `MustBeFoundHere`, specify what you want to find in `{}` | ",
+                MESSAGE_WITH_EXTRA_FROM_CLASS_PY
+            ));
+        }
+    }
+
+    make_errors::error_or_ok(&obj, extra_values, rule_status, flag)
+}
+
+fn just_look_at_this_hard(
+    py: Python,
+    slf: &TemplateValidator,
+    regex: &String,
+    rule_status: &RuleStatus,
+    text: &str,
+) -> PyResult<()> {
+    let obj = slf.python_classes[&rule_status.id].to_object(py);
+    let obj_for_extra = obj.downcast::<types::PyType>(py).unwrap();
+    let extra_names =
+        make_errors::extra_from_class(obj_for_extra, MESSAGE_WITH_EXTRA_FROM_CLASS_PY)?;
+
+    let mut extra_values = HashMap::new();
+    let mut flag = false;
+    for capture in check_convert::convert::string_to_fancy_regex(regex).captures_iter(text) {
+        flag = true;
+        for name in &extra_names {
+            match capture.as_ref().unwrap().name(&name) {
                 Some(value) => {
                     extra_values.insert(name.to_string(), value.as_str().to_string());
                 }
