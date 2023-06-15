3232c40a3c2ff2cba459297bf7bdb4d6a25bbccd
Mansur Azatbek <mansur62624@gmail.com>
2023-06-15 13:14:25 +0600

    feat: baseError template

--- src/base_error.rs
+++ src/base_error.rs
@@ -1,62 +1,42 @@
-use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use std::collections::HashMap;
 
 #[pyclass]
 #[derive(Debug)]
-/// Структура которая будет являться шаблоном класса в `python` для создание собвственных ошибок
+/// Структура которая будет являться шаблоном класса в `python` для создание собственных ошибок
 pub struct BaseError {
     message: String,
     extra: HashMap<String, String>,
     rules: HashMap<String, usize>,
 }
-
-#[pyclass(subclass)]
-#[derive(Debug)]
-/// Обертка для исключения Python
-pub struct BaseErrorWrapper {
-    inner: PyObject,
-}
-
-/// Реализация методов для `BaseErrorWrapper`
 #[pymethods]
-impl BaseErrorWrapper {
-    /// Создаем экземпляр обертки с заданным исключением
+impl BaseError {
     #[new]
-    pub fn new(inner: PyObject) -> Self {
-        BaseErrorWrapper { inner }
+    #[pyo3(signature = (message = None, extra = None, rules = None))]
+    pub fn new(
+        message: Option<String>,
+        extra: Option<HashMap<String, String>>,
+        rules: Option<HashMap<String, usize>>,
+    ) -> Self {
+        BaseError {
+            message: message.unwrap_or_default(),
+            extra: extra.unwrap_or_default(),
+            rules: rules.unwrap_or_default(),
+        }
     }
-    #[staticmethod]
-    pub fn __base__(_py: Python<'_>) -> PyResult<PyObject> {
-        // Python::with_gil(|py| {
-        let base_exception = PyModule::new(_py, "builtins")?.getattr("BaseException")?;
-        Ok(base_exception.to_object(_py))
-        // })
-    }
-}
 
-/// Реализация преобразования из `BaseErrorWrapper` в `PyErr`
-/// Для использования обернутого исключения в Python
-impl From<BaseErrorWrapper> for PyErr {
-    fn from(wrapper: BaseErrorWrapper) -> PyErr {
-        PyErr::new::<PyException, _>(wrapper.inner)
+    #[getter]
+    pub fn get_message(&self) -> PyResult<String> {
+        Ok(self.message.clone())
     }
-}
-/// Функция для создания экземпляра обертки `BaseErrorWrapper`
-#[pyfunction]
-pub fn throw_base_error(
-    _py: Python<'_>,
-    message: String,
-    extra: HashMap<String, String>,
-    rules: HashMap<String, usize>,
-) -> PyResult<BaseErrorWrapper> {
-    Python::with_gil(|py| {
-        let base_error = BaseError {
-            message,
-            extra,
-            rules,
-        };
-        let pyerr: PyErr = PyErr::new::<BaseError, _>(base_error);
-        Ok(BaseErrorWrapper::new(pyerr.to_object(py)))
-    })
+
+    #[getter]
+    pub fn get_extra(&self) -> PyResult<HashMap<String, String>> {
+        Ok(self.extra.clone())
+    }
+
+    #[getter]
+    pub fn get_rules(&self) -> PyResult<HashMap<String, usize>> {
+        Ok(self.rules.clone())
+    }
 }
