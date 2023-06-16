b9a419aed730016406a492afa21fed8878b12fa5
Mansur Azatbek <mansur62624@gmail.com>
2023-06-16 13:45:42 +0600

    chore: changed api check

--- tests/template.py
+++ tests/template.py
@@ -1,32 +1,65 @@
 import pystval
 import asyncio
-from pystval import TemplateValidator, It, PystvalError
+from pystval import TemplateValidator, MatchRequirement
 
-print(PystvalError.__base__)
+
+class PystvalErrorMeta(type):
+    def __new__(cls, name, bases, attrs):
+        if name == 'PystvalError':
+            attrs['allow_default_values'] = True
+        else:
+            if 'message' not in attrs or not attrs['message']:
+                raise NotImplementedError(
+                    "Subclasses must provide a non-empty 'message' attribute.")
+            if 'rules' not in attrs or not isinstance(attrs['rules'], dict):
+                raise NotImplementedError(
+                    "Subclasses must provide a 'rules' attribute of type 'dict'.")
+        return super().__new__(cls, name, bases, attrs)
+
+
+class PystvalError(Exception, metaclass=PystvalErrorMeta):
+
+    def __init__(self, message: str = None, rules: dict[str, enumerate] = None, **extra):
+        self._extra = extra
+        self._rules = rules
+        if message is None:
+            self._message = self.message.format(**extra)
+
+    @property
+    def message(self):
+        return self._message
+
+    @property
+    def extra(self):
+        return self._extra
+
+    @property
+    def rules(self):
+        return self._rules
 
 
 class FindSomething(PystvalError):
     message = "Gotcha : {OS}"
     rules = {
-        r"""(?P<OS>Linux)""": It.MustBeFoundHere,
+        r"""(?P<OS>Linux)""": MatchRequirement.NotToBeFoundHere,
     }
 
 # # ============================================
 
 
-# async def init():
-#     # text = await get_bytes("-- link --")
-#     with open('Makefile', 'rb') as file:
-#         text = file.read()
-#     validator_sample = TemplateValidator(flags=[FindSomething])
-#     try:
-#         await validator_sample.validate(text)
-#     except PystvalError as e:
-#         print(f"ERROR VALIDATE: '{e.message}'")
+async def init():
+    # text = await get_bytes("-- link --")
+    with open('Makefile', 'rb') as file:
+        text = file.read()
+    validator_sample = TemplateValidator(flags=[FindSomething])
+    try:
+        await validator_sample.validate(text)
+    except PystvalError as e:
+        print(f"ERROR VALIDATE: '{e.extra}'")
 
 
-# # # # =============================================
-# loop = asyncio.get_event_loop()
-# task = loop.create_task(init())
-# loop.run_until_complete(task)
-# # ============================================
+# # # =============================================
+loop = asyncio.get_event_loop()
+task = loop.create_task(init())
+loop.run_until_complete(task)
+# ============================================
