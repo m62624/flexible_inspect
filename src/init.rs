8fd134dd6d7e96dd7a66dc66edd8c9955b6a2650
Mansur Azatbek <mansur62624@gmail.com>
2023-06-19 00:48:17 +0600

    chore: new scheme for nestedstring

--- src/init.rs
+++ src/init.rs
@@ -1,102 +1,40 @@
 use super::*;
-use pyo3::exceptions;
-// Расфасовка данных
-pub fn data_unpackaging(
-    py: Python,
-    obj: PyObject,
-    python_classes: &mut HashMap<usize, PyObject>,
-    all_simple_rules: &mut HashMap<String, RuleStatus>,
-    all_hard_rules: &mut HashMap<String, RuleStatus>,
-    selected_simple_rules: &mut Vec<String>,
-) -> PyResult<()> {
-    // Проверяем что переданый объект является списком, иначе ошибка типа переменной
-    if let Ok(dict) = obj.downcast::<types::PyList>(py) {
-        let mut id_class: usize = 0;
-        for class_py in dict {
-            // Проверяем что элемент списка является классом, иначе ошибка типа переменной
-            if let Ok(class_py) = class_py.downcast::<types::PyType>() {
-                get_any_regex_from_class(
-                    class_py,
-                    id_class,
-                    all_simple_rules,
-                    all_hard_rules,
-                    selected_simple_rules,
-                )?;
-                python_classes.insert(id_class, class_py.to_object(py));
-
-                id_class += 1;
-            } else {
-                return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                    "'{}' must be a 'Class'",
-                    class_py
-                )));
-            }
+#[pymethods]
+impl Rule {
+    #[new]
+    /// Создание корня дерева
+    pub fn new(inner: String, requirements: MatchRequirement) -> Self {
+        Rule {
+            inner,
+            requirements,
+            rule_for_the_rule: HashMap::new(),
         }
-    } else {
-        return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-            "'{}' must be a 'List[ Class, Class... ]'",
-            obj
-        )));
     }
-    Ok(())
+    /// Добавление дочерней вложенной строки
+    pub fn extend(&mut self, key: &str, child: Rule) {
+        self.rule_for_the_rule.insert(key.to_owned(), child);
+    }
+}
+impl<'a> RuleIterator<'a> {
+    pub fn new(root: &'a Rule) -> Self {
+        let mut stack = Vec::new();
+        stack.push(root);
+
+        RuleIterator { stack }
+    }
 }
 
-// Получаем все регулярные выражения из класса
-pub fn get_any_regex_from_class(
-    class_py: &types::PyType,
-    id_class: usize,
-    all_simple_rules: &mut HashMap<String, RuleStatus>,
-    all_hard_rules: &mut HashMap<String, RuleStatus>,
-    selected_simple_rules: &mut Vec<String>,
-) -> PyResult<()> {
-    // Проверяем что класс имеет атрибут RULES_FROM_CLASS_PY, иначе ошибка атрибута
-    if let Ok(py_dict) = class_py.getattr(RULES_FROM_CLASS_PY) {
-        // Проверяем что атрибут является словарем, иначе ошибка типа переменной
-        if let Ok(dict) = py_dict.downcast::<types::PyDict>() {
-            for (key, value) in dict {
-                // Проверяем что ключ является строкой, иначе ошибка типа переменной
-                if let Ok(key) = key.extract::<String>() {
-                    // Проверяем что значение является Enum, иначе ошибка типа переменной
-                    if let Ok(value) = value.extract::<MatchRequirement>() {
-                        // Проверяем что ключ является валидным регулярным выражением (простого типа)
-                        if check_convert::check::is_default_regex_fisrt_step(&key) {
-                            all_simple_rules
-                                .insert(key.to_string(), RuleStatus::new(id_class, value));
-                            selected_simple_rules.push(key);
-                            // Проверяем что ключ является валидным регулярным выражением (сложного типа)
-                        } else if check_convert::check::is_fancy_regex_second_step(&key) {
-                            all_hard_rules
-                                .insert(key.to_string(), RuleStatus::new(id_class, value));
-                        } else {
-                            return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                                "{} --- Invalid regular expression",
-                                key
-                            )));
-                        }
-                    } else {
-                        return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                            "'{}' must be a 'Enum'",
-                            value
-                        )));
-                    }
-                } else {
-                    return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                        "'{}' must be a 'String'",
-                        key
-                    )));
-                }
+impl<'a> Iterator for RuleIterator<'a> {
+    type Item = &'a Rule;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.stack.pop() {
+            for child in node.rule_for_the_rule.values() {
+                self.stack.push(child);
             }
+            Some(node)
         } else {
-            return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                "'{}' must be a 'dict {{ Class: Enum }}'",
-                py_dict
-            )));
+            None
         }
-    } else {
-        return Err(PyErr::new::<exceptions::PyAttributeError, _>(format!(
-            "There is no '{}' attribute for getting rules",
-            RULES_FROM_CLASS_PY
-        )));
     }
-    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,171 +1,32 @@
-//! `Pystval` - `Rust` библиотека для `Python`. Выполняет валидацию данных (строки) с помощью регулярных выражений
-
-//===============================================================================
-// имя модуля для `Python`
-pub const MODULE_NAME: &str = "pystval";
-// имя атрибута, где хранится само сообщение и **extra переменные из класса Python
-pub const MESSAGE_WITH_EXTRA_FROM_CLASS_PY: &str = "message";
-// имя атрибута, где хранится регулярные выражения из класса Python
-pub const RULES_FROM_CLASS_PY: &str = "rules";
-// имя атрибута, где хранится **extra переменные для заполнения из класса Python
-pub const EXTRA_FROM_CLASS_PY: &str = "extra";
-// имя класса ошибки (базовый шаблон) для `Python`
-pub const BASE_ERROR: &str = "PystvalError";
-//===============================================================================
-
-// Проверка и конвертация данных из Python в Rust и обратно
-mod check_convert;
-// Компоненты необходимые для конструктора `TemplateValidator`
+use pyo3::prelude::*;
+use std::collections::HashMap;
 mod init;
-// Базовый класс для кастомных ошибок
-mod base_error;
-// Создание и получение данных ошибок
-mod make_errors;
-// Компоненты для метода, где проходит сама валидация
-mod validate;
-// Все юнит тесты выненсены в отдельный модуль
-mod unit_tests;
-
-// Отвечает за взаимодействие сборщика мусора СPython с Rust
-use pyo3::gc::{PyTraverseError, PyVisit};
-// Используем для получения данных из Python
-use pyo3::{prelude::*, types};
-use std::{collections::HashMap, str};
-/// Перечечисление, где даны варианты действия при положительном результате регулярных выражений
+/// Перечисления для определения требований к строке
 #[pyclass]
 #[derive(Debug, Clone)]
 pub enum MatchRequirement {
-    /// * `MustBeFoundHere` - Должно быть найдено, иначе будет вызвано исключение
-    MustBeFoundHere,
-    /// * `NotToBeFoundHere` - Не должно быть найдено, иначе будет вызвано исключение
-    NotToBeFoundHere,
+    MustBeFound,
+    MustNotBefound,
 }
 
-/// Структура для хранения ошибок и статуса
-#[derive(Debug, Clone)]
-pub struct RuleStatus {
-    id: usize,
-    status: MatchRequirement,
-}
-
-impl RuleStatus {
-    pub fn new(id: usize, status: MatchRequirement) -> Self {
-        Self { id, status }
-    }
-}
-
-/// Структруа для хранения ошибок и регулярных выражений\
-/// Является шаблоном для создание *уникальных* валидаторов
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct TemplateValidator {
     // хранит все ошибки ( KEY: `ID` и VALUE: `PyError` )
-    python_classes: HashMap<usize, PyObject>,
-    // хранит default regex (KEY `Regex` и VALUE `ID CLASS & STATUS`)
-    all_simple_rules: HashMap<String, RuleStatus>,
-    // хранит fancy regex (KEY `Regex` и VALUE `ID CLASS & STATUS`)
-    all_hard_rules: HashMap<String, RuleStatus>,
-    // Собираем все default regex и *одним проходом* проверяем всё
-    selected_simple_rules: regex::RegexSet,
+    py_error_classes: HashMap<usize, PyObject>,
+    rules_from_py_error_classes: HashMap<usize, Rule>,
 }
 
-// Реализация методов для TemplateValidator которые будут доступны в `Python`
-#[pymethods]
-impl TemplateValidator {
-    /// Создаем экземлпяр с заданными параметрами проверки\
-    /// Принимает `PyDict {class,status_throw}`\
-    /// **Может принимать сразу `class` без экземпляра**
-    #[new]
-    pub fn __new__(flags: PyObject) -> PyResult<Self> {
-        Python::with_gil(|py| -> PyResult<Self> {
-            let mut python_classes: HashMap<usize, PyObject> = HashMap::new();
-            let mut all_simple_rules: HashMap<String, RuleStatus> = HashMap::new();
-            let mut all_hard_rules: HashMap<String, RuleStatus> = HashMap::new();
-            let mut selected_simple_rules: Vec<String> = Vec::new();
-            init::data_unpackaging(
-                py,
-                flags,
-                &mut python_classes,
-                &mut all_simple_rules,
-                &mut all_hard_rules,
-                &mut selected_simple_rules,
-            )?;
-            let rsl_info = Self {
-                all_hard_rules,
-                all_simple_rules,
-                python_classes,
-                selected_simple_rules: regex::RegexSet::new(selected_simple_rules).unwrap(),
-            };
-            // dbg!(&rsl_info);
-            Ok(rsl_info)
-        })
-    }
-
-    /// Служит для запуска async метода
-    /// Метод `async_validate` используется для запуска асинхронной функции `core_validate` внутри. Это позволяет выполнять асинхронную валидацию внутри `Python`
-    #[cfg(not(tarpaulin_include))]
-    #[pyo3(name = "async_validate")]
-    fn async_validate<'py>(
-        &self,
-        py: Python<'py>,
-        text_bytes: &types::PyBytes,
-    ) -> PyResult<&'py PyAny> {
-        let unsafe_self = unsafe { &*(self as *const Self) };
-        let text = check_convert::convert::bytes_to_string_utf8(text_bytes.as_bytes())?;
-        pyo3_asyncio::async_std::future_into_py(py, async move {
-            unsafe_self.core_validate(text)?;
-            Ok(Python::with_gil(|py| py.None()))
-        })
-    }
-
-    /// Метод `validate` позволяет выполнять валидацию внутри `Python`
-    #[cfg(not(tarpaulin_include))]
-    #[pyo3(name = "validate")]
-    fn validate<'py>(&self, text_bytes: &types::PyBytes) -> PyResult<()> {
-        self.core_validate(check_convert::convert::bytes_to_string_utf8(
-            text_bytes.as_bytes(),
-        )?)
-    }
-    //================== (РАБОТАЕТ ТОЛЬКО С `C API` (CPYTHON))==================
-    /*
-    Метод `__traverse__` используется для рекурсивного обхода объекта и уведомления `Python` о всех
-    вложенных объектах, которые должны быть добавлены в механизм управления памятью `Python`. В этой
-    реализации метод `__traverse__` проходится по всем значениям в хэш-таблице `python_classes` и вызывает
-    `visit.call()` для каждого объекта типа `PyObject` в свойстве `error` объекта типа `RuleStatus`.
-    Это гарантирует, что объекты типа `PyObject` не будут освобождены Python'ом до тех пор,
-    пока они не перестанут использоваться в `Rust`.
-     */
-    #[cfg(not(tarpaulin_include))]
-    fn __traverse__(&self, visit: PyVisit<'_>) -> Result<(), PyTraverseError> {
-        for class_py in self.python_classes.values() {
-            visit.call(class_py)?;
-        }
-        Ok(())
-    }
-
-    /*
-    Метод `__clear__` используется для освобождения памяти, занятой объектом. В этой реализации метод
-    `__clear__` очищает хэш-таблицу `python_classes`, что приводит к уменьшению счетчика ссылок на каждый
-    объект типа PyObject в свойстве error объекта типа `RuleStatus`. Если количество ссылок на объект
-    достигнет нуля, он будет автоматически удален Python'ом.
-     */
-    #[cfg(not(tarpaulin_include))]
-    fn __clear__(&mut self) {
-        // Удаляем все значения из HashMap, тем самым снижая счетчик ссылок на PyObject
-        self.python_classes.clear();
-    }
-    //================== (РАБОТАЕТ ТОЛЬКО С `C API` (CPYTHON))==================
+/// Структура для хранения вложенных строк
+#[pyclass]
+#[derive(Debug, Clone)]
+pub struct Rule {
+    pub inner: String,
+    pub requirements: MatchRequirement,
+    pub rule_for_the_rule: HashMap<String, Rule>,
 }
 
-// Импортируем всё необходимое в `Python`
-#[cfg(not(tarpaulin_include))]
-mod export {
-    use super::*;
-    #[pymodule]
-    fn pystval(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-        m.add_class::<TemplateValidator>()?;
-        m.add_class::<MatchRequirement>()?;
-        PyModule::from_code(_py, &base_error::get_base_error(), "", MODULE_NAME)?;
-        Ok(())
-    }
+/// Итератор для обхода дерева в глубину
+pub struct RuleIterator<'a> {
+    stack: Vec<&'a Rule>,
 }
