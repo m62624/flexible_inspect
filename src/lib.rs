75ba3577212a4b1c5064f76aec54d6aa30aefddb
Mansur Azatbek <mansur62624@gmail.com>
2023-06-19 10:51:34 +0600

    feat: added global consts

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,18 @@ mod contrainer_tree;
 mod init;
 mod match_requirement;
 mod rule;
+
+// имя модуля для `Python`
+pub const MODULE_NAME: &str = "pystval";
+// имя атрибута, где хранится само сообщение и **extra переменные из класса Python
+pub const MESSAGE_WITH_EXTRA_FROM_CLASS_PY: &str = "message";
+// имя атрибута, где хранится регулярные выражения из класса Python
+pub const RULES_FROM_CLASS_PY: &str = "rules";
+// имя атрибута, где хранится **extra переменные для заполнения из класса Python
+pub const EXTRA_FROM_CLASS_PY: &str = "extra";
+// имя класса ошибки (базовый шаблон) для `Python`
+pub const BASE_ERROR: &str = "PystvalError";
+
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct TemplateValidator {
