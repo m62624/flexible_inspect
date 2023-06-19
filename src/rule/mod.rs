39e59c7f9a26e0375971a0eae9e1cab15cfcc36a
Mansur Azatbek <mansur62624@gmail.com>
2023-06-19 19:44:47 +0600

    test: check api

--- src/rule/mod.rs
+++ src/rule/mod.rs
@@ -1,27 +1,58 @@
+use std::f32::consts::E;
+
 use super::*;
+/// Импорт модуля с имплементацией `__eq__`
 mod impl_eq;
+/// Импорт модуля с имплементацией `__hash__`
 mod impl_hash;
+use match_requirement::MatchRequirement;
+use pyo3::{exceptions, types};
 /// Структура для хранения вложенных строк
+/// Ставим всё в Option, чтобы можно было использовать `take`
+/// `take` - забирает значение из переменной, а вместо него ставит `None`
+/// тем самым мы сможем каждый раз перемещать значение из переменной в переменную без копирования
 #[pyclass]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Rule {
-    inner: String,
-    requirements: match_requirement::MatchRequirement,
-    rules_for_the_rule: HashMap<String, Rule>,
+    /// Строка является Regex выражением
+    inner: Option<String>,
+    /// Какое требование при нахождении совпадений
+    requirements: Option<MatchRequirement>,
+    /// Вложенные правила, которые будут проверяться, если данное правило сработало
+    rules_for_the_rule: Option<Vec<Rule>>,
 }
+
 #[pymethods]
 impl Rule {
     #[new]
     /// Создание корня дерева
-    pub fn new(inner: String, requirements: match_requirement::MatchRequirement) -> Self {
+    pub fn new(inner: String, requirements: MatchRequirement) -> Self {
         Rule {
-            inner,
-            requirements,
-            rules_for_the_rule: HashMap::new(),
+            inner: Some(inner),
+            requirements: Some(requirements),
+            rules_for_the_rule: Some(Vec::new()),
         }
     }
     /// Добавление дочерней строки
-    pub fn extend(&mut self, key: String, child: Rule) {
-        self.rules_for_the_rule.insert(key, child);
+    pub fn extend(&mut self, py: Python<'_>, nested_rules: PyObject) -> PyResult<Self> {
+        // Проверяем, что это список
+        if let Ok(list) = nested_rules.downcast::<types::PyList>(py) {
+            // Итерируемся по списку для получения всех дочерних правил
+            for list in list.iter() {
+                // Проверяем, что это правило
+                if let Ok(rule) = list.extract::<Rule>() {
+                    // Добавляем в вектор дочерних правил
+                    self.rules_for_the_rule.as_mut().unwrap().push(rule);
+                } else {
+                    return Err(PyErr::new::<exceptions::PyTypeError, _>("Expected Rule"));
+                }
+            }
+            // Возвращаем саму структуру
+            return Ok(std::mem::take(self));
+        }
+        return Err(PyErr::new::<exceptions::PyTypeError, _>("Expected List"));
+    }
+    pub fn show_tree(&self) {
+        println!("{:#?}", self);
     }
 }
--- tests/template.py
+++ tests/template.py
@@ -1,65 +1,17 @@
 import pystval
 import asyncio
-from pystval import TemplateValidator, MatchRequirement
+from pystval import TemplateValidator, MatchRequirement,Rule
 
+def check():
+    Rule("Root rule",MatchRequirement.MustBeFound).extend([
+        Rule("Subrule-1 of Root",MatchRequirement.MustBeFound),
+        Rule("Subrule-2 of Root",MatchRequirement.MustBeFound),
+        Rule("Subrule-3 of Root",MatchRequirement.MustBeFound),
+        Rule("Subrule-4 of Root",MatchRequirement.MustBeFound).extend([
+            Rule("Subrule-1 of Subrule-4",MatchRequirement.MustBeFound),
+            Rule("Subrule-2 of Subrule-4",MatchRequirement.MustBeFound),
+            Rule("Subrule-3 of Subrule-4",MatchRequirement.MustBeFound),
+        ])
+        ]).show_tree()
 
-class PystvalErrorMeta(type):
-    def __new__(cls, name, bases, attrs):
-        if name == 'PystvalError':
-            attrs['allow_default_values'] = True
-        else:
-            if 'message' not in attrs or not attrs['message']:
-                raise NotImplementedError(
-                    "Subclasses must provide a non-empty 'message' attribute.")
-            if 'rules' not in attrs or not isinstance(attrs['rules'], dict):
-                raise NotImplementedError(
-                    "Subclasses must provide a 'rules' attribute of type 'dict'.")
-        return super().__new__(cls, name, bases, attrs)
-
-
-class PystvalError(Exception, metaclass=PystvalErrorMeta):
-
-    def __init__(self, message: str = None, rules: dict[str, enumerate] = None, **extra):
-        self._extra = extra
-        self._rules = rules
-        if message is None:
-            self._message = self.message.format(**extra)
-
-    @property
-    def message(self):
-        return self._message
-
-    @property
-    def extra(self):
-        return self._extra
-
-    @property
-    def rules(self):
-        return self._rules
-
-
-class FindSomething(PystvalError):
-    message = "Gotcha : {OS}"
-    rules = {
-        r"""(?P<OS>Linux)""": MatchRequirement.NotToBeFoundHere,
-    }
-
-# # ============================================
-
-
-async def init():
-    # text = await get_bytes("-- link --")
-    with open('Makefile', 'rb') as file:
-        text = file.read()
-    validator_sample = TemplateValidator(flags=[FindSomething])
-    try:
-        await validator_sample.validate(text)
-    except PystvalError as e:
-        print(f"ERROR VALIDATE: '{e.extra}'")
-
-
-# # # =============================================
-loop = asyncio.get_event_loop()
-task = loop.create_task(init())
-loop.run_until_complete(task)
-# ============================================
+check()
