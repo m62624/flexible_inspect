6b9bd4970ec5dc92aa53257bd987fc2b1117c436
Mansur Azatbek <mansur62624@gmail.com>
2023-06-21 16:33:19 +0600

    feat: added fn `find_capture_*`

---
+++ src/template_validator/regex_capture.rs
@@ -0,0 +1,16 @@
+use super::*;
+impl TemplateValidator {
+    /// Поиск совпадений, используя `default regex` 
+    fn find_capture_default<'a>(pattern: &'a str, text: &'a str) -> Vec<regex::Captures<'a>> {
+        check_convert::convert::string_to_default_regex(pattern)
+            .captures_iter(text)
+            .collect::<Vec<_>>()
+    }
+    /// Поиск совпадений, используя `fancy regex` 
+    fn find_capture_fancy<'a>(pattern: &'a str, text: &'a str) -> Vec<fancy_regex::Captures<'a>> {
+        check_convert::convert::string_to_fancy_regex(pattern)
+            .captures_iter(text)
+            .filter_map(|result| result.ok())
+            .collect::<Vec<_>>()
+    }
+}
