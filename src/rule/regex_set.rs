7a1425f8f329489b1bd3150039ac9d49fae835b2
Mansur Azatbek <mansur62624@gmail.com>
2023-06-24 01:51:30 +0600

    refactor(`RegexSet`): changed return type (`Vec<usize>`)

--- src/rule/regex_set.rs
+++ src/rule/regex_set.rs
@@ -1,6 +1,6 @@
 use super::*;
 impl Rule {
-    pub fn init_root_regex_set(rules: &Vec<(rule::Rule, usize)>) -> regex::RegexSet {
+    pub fn init_root_regex_set(rules: &Vec<(rule::Rule, usize)>, text: &str) -> Vec<usize> {
         regex::RegexSet::new(
             rules
                 .iter()
@@ -15,9 +15,12 @@ impl Rule {
                 .collect::<Vec<&Box<str>>>(),
         )
         .unwrap()
+        .matches(text)
+        .iter()
+        .collect::<Vec<_>>()
     }
 
-    pub fn make_sub_regex_set(subrules: &Option<Vec<Rule>>) -> Option<regex::RegexSet> {
+    pub fn make_sub_regex_set(subrules: &Option<Vec<Rule>>, text: &str) -> Option<Vec<usize>> {
         if let Some(subrules) = subrules {
             return Some(
                 regex::RegexSet::new(
@@ -33,9 +36,33 @@ impl Rule {
                         })
                         .collect::<Vec<&Box<str>>>(),
                 )
-                .unwrap(),
+                .unwrap()
+                .matches(text)
+                .iter()
+                .collect::<Vec<_>>(),
             );
         }
         None
     }
+}
+
+#[test]
+fn show_root_rule() {
+    pyo3::prepare_freethreaded_python();
+    Python::with_gil(|py| {
+        dbg!(rule::Rule::make_sub_regex_set(
+            &Some(vec![
+                Rule::new(
+                    "root 5 ^a(?>bc|b)c$".to_string(),
+                    MatchRequirement::MustBeFound
+                )
+                .unwrap(),
+                Rule::new("root - AV".to_string(), MatchRequirement::MustBeFound).unwrap(),
+                Rule::new("root 2".to_string(), MatchRequirement::MustBeFound).unwrap(),
+                Rule::new("root 3".to_string(), MatchRequirement::MustBeFound).unwrap(),
+                Rule::new("root 4".to_string(), MatchRequirement::MustBeFound).unwrap(),
+            ]),
+            "root - AV root 2 root"
+        ));
+    });
 }
