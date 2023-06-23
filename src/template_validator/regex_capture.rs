3db8fb3d71ed81aa0d9fc49b26494ef51dd4f053
Mansur Azatbek <mansur62624@gmail.com>
2023-06-23 21:07:06 +0600

    refactor: changed iterators and return types

--- src/template_validator/regex_capture.rs
+++ src/template_validator/regex_capture.rs
@@ -1,19 +1,29 @@
 use super::*;
 impl TemplateValidator {
     /// Поиск совпадений, используя `default regex`
-    pub fn find_capture_default<'a>(pattern: &'a str, text: &'a str) -> Vec<regex::Captures<'a>> {
+    pub fn find_default_capture<'a>(pattern: &'a str, text: &'a str) -> Vec<&'a str> {
         check_convert::convert::string_to_default_regex(pattern)
             .captures_iter(text)
-            .collect::<Vec<_>>()
+            .flat_map(|captures| {
+                captures
+                    .iter()
+                    .filter_map(|capture| capture.map(|value| value.as_str()))
+                    .collect::<Vec<_>>()
+            })
+            .collect()
     }
+
     /// Поиск совпадений, используя `fancy regex`
-    pub fn find_capture_fancy<'a>(
-        pattern: &'a str,
-        text: &'a str,
-    ) -> Vec<fancy_regex::Captures<'a>> {
+    pub fn find_fancy_capture<'a>(pattern: &'a str, text: &'a str) -> Vec<&'a str> {
         check_convert::convert::string_to_fancy_regex(pattern)
             .captures_iter(text)
             .filter_map(|result| result.ok())
-            .collect::<Vec<_>>()
+            .flat_map(|captures| {
+                captures
+                    .iter()
+                    .filter_map(|capture| capture.map(|value| value.as_str()))
+                    .collect::<Vec<_>>()
+            })
+            .collect()
     }
 }
