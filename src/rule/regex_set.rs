00f6fc5df0706a26f5e799a19f575181af5ee93b
Mansur Azatbek <mansur62624@gmail.com>
2023-06-24 01:37:51 +0600

    refactor(`RegexSet`): changed return type

--- src/rule/regex_set.rs
+++ src/rule/regex_set.rs
@@ -1,33 +1,39 @@
 use super::*;
 impl Rule {
-    pub fn init_root_regex_set<'a>(rules: &'a Vec<(rule::Rule, usize)>) -> Vec<&'a Box<str>> {
-        rules
-            .iter()
-            .filter_map(|rule| {
-                if let Some(value) = &rule.0.rule_raw {
-                    if let regex_types::RGX::Default = value.1 {
-                        return Some(&value.0);
+    pub fn init_root_regex_set(rules: &Vec<(rule::Rule, usize)>) -> regex::RegexSet {
+        regex::RegexSet::new(
+            rules
+                .iter()
+                .filter_map(|rule| {
+                    if let Some(value) = &rule.0.rule_raw {
+                        if let regex_types::RGX::Default = value.1 {
+                            return Some(&value.0);
+                        }
                     }
-                }
-                None
-            })
-            .collect()
+                    None
+                })
+                .collect::<Vec<&Box<str>>>(),
+        )
+        .unwrap()
     }
 
-    pub fn make_sub_regex_set<'a>(subrules: &'a Option<Vec<Rule>>) -> Option<Vec<&'a Box<str>>> {
+    pub fn make_sub_regex_set(subrules: &Option<Vec<Rule>>) -> Option<regex::RegexSet> {
         if let Some(subrules) = subrules {
             return Some(
-                subrules
-                    .iter()
-                    .filter_map(|value| {
-                        if let Some(value) = &value.rule_raw {
-                            if let regex_types::RGX::Default = value.1 {
-                                return Some(&value.0);
+                regex::RegexSet::new(
+                    subrules
+                        .iter()
+                        .filter_map(|value| {
+                            if let Some(value) = &value.rule_raw {
+                                if let regex_types::RGX::Default = value.1 {
+                                    return Some(&value.0);
+                                }
                             }
-                        }
-                        None
-                    })
-                    .collect(),
+                            None
+                        })
+                        .collect::<Vec<&Box<str>>>(),
+                )
+                .unwrap(),
             );
         }
         None
