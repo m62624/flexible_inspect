c7c58805f518d3e006c06f8663c7d1b79ed11497
Mansur Azatbek <mansur62624@gmail.com>
2023-06-24 12:53:11 +0600

    refactor: divided by the `regex rule` collections

--- src/template_validator/validate.rs
+++ src/template_validator/validate.rs
@@ -1,22 +1,4 @@
-use crate::rule::regex_types;
-
 use super::*;
-
-impl TemplateValidator {
-    pub fn core_validate_single(&self, py: Python, text: &str) -> PyObject {
-        // Список ошибок
-        let list_errors = types::PyList::new::<PyObject, _>(py, []);
-        // Проверка по каждому правилу
-        self.all_rules
-            .iter()
-            .map(|rule_container| {
-
-                // rule.0.get_inner().
-            })
-            .for_each(drop);
-        return list_errors.to_object(py);
-    }
-}
 
 pub fn action_on_the_matches(matches: &Vec<&str>, rule_contrainer: &rule::Rule) -> PyResult<bool> {
     match rule_contrainer.get_requirement()? {
