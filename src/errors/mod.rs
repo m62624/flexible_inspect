0e19f03fbb0cf538b971464385548f7e447d4539
Mansur Azatbek <mansur62624@gmail.com>
2023-06-25 01:04:14 +0600

    feat: added modules

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,8 +1,8 @@
 use super::{
     BASE_ERROR, EXTRA_FROM_CLASS_PY, MESSAGE_WITH_EXTRA_FROM_CLASS_PY, RULES_FROM_CLASS_PY,
 };
-mod extra_collection;
-mod make_error;
+pub mod extra_collection;
+pub mod make_error;
 /// Функция для получения базового класса ошибки
 // Почему именно такая реализация ?
 /*
