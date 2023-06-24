9c9367e5e02519fa4ddd4f31660b05bab8122dd4
Mansur Azatbek <mansur62624@gmail.com>
2023-06-24 12:51:54 +0600

    refactor: changed return type

--- src/template_validator/regex_capture.rs
+++ src/template_validator/regex_capture.rs
@@ -1,29 +1,21 @@
 use super::*;
 impl TemplateValidator {
     /// Поиск совпадений, используя `default regex`
-    pub fn find_default_capture<'a>(pattern: &'a str, text: &'a str) -> Vec<&'a str> {
-        check_convert::convert::string_to_default_regex(pattern)
+    pub fn find_default_capture<'a>(pattern: &'a str, text: &'a str) -> Vec<regex::Captures<'a>> {
+        check_convert::convert::str_to_default_regex(pattern)
             .captures_iter(text)
-            .flat_map(|captures| {
-                captures
-                    .iter()
-                    .filter_map(|capture| capture.map(|value| value.as_str()))
-                    .collect::<Vec<_>>()
-            })
+            .map(|captures| captures)
             .collect()
     }
 
     /// Поиск совпадений, используя `fancy regex`
-    pub fn find_fancy_capture<'a>(pattern: &'a str, text: &'a str) -> Vec<&'a str> {
-        check_convert::convert::string_to_fancy_regex(pattern)
+    pub fn find_fancy_capture<'a>(
+        pattern: &'a str,
+        text: &'a str,
+    ) -> Vec<fancy_regex::Captures<'a>> {
+        check_convert::convert::str_to_fancy_regex(pattern)
             .captures_iter(text)
-            .filter_map(|result| result.ok())
-            .flat_map(|captures| {
-                captures
-                    .iter()
-                    .filter_map(|capture| capture.map(|value| value.as_str()))
-                    .collect::<Vec<_>>()
-            })
+            .filter_map(|captures| captures.ok())
             .collect()
     }
 }
