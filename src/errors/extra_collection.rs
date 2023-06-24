83376e564a9ad608853763b7ccffab8b781cd827
Mansur Azatbek <mansur62624@gmail.com>
2023-06-25 01:02:11 +0600

    feat: added functions

--- src/errors/extra_collection.rs
+++ src/errors/extra_collection.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 use pyo3::prelude::*;
 use pyo3::types;
@@ -21,4 +23,57 @@ pub fn extra_from_class<'a>(
     } else {
         Ok(Vec::new())
     }
+}
+
+pub fn extra_from_default_capture<'a>(
+    extra_name: Vec<&'a str>,
+    captures: Vec<regex::Captures<'a>>,
+) -> Option<HashMap<&'a str, &'a str>> {
+    let mut extra_values: HashMap<&str, &str> = HashMap::new();
+    captures
+        .iter()
+        .map(|capture| {
+            extra_name
+                .iter()
+                .map(|name| match capture.name(name) {
+                    Some(value) => {
+                        extra_values.insert(name, value.as_str());
+                    }
+                    None => {
+                        extra_values.insert(name, "___");
+                    }
+                })
+                .for_each(drop);
+        })
+        .for_each(drop);
+    if !extra_values.is_empty() {
+        return Some(extra_values);
+    }
+    return None;
+}
+pub fn extra_from_default_fancy<'a>(
+    extra_name: &'a Vec<&'a str>,
+    captures: Vec<fancy_regex::Captures<'a>>,
+) -> Option<HashMap<&'a str, &'a str>> {
+    let mut extra_values: HashMap<&str, &str> = HashMap::new();
+    captures
+        .iter()
+        .map(|capture| {
+            extra_name
+                .iter()
+                .map(|name| match capture.name(name) {
+                    Some(value) => {
+                        extra_values.insert(name, value.as_str());
+                    }
+                    None => {
+                        extra_values.insert(name, "___");
+                    }
+                })
+                .for_each(drop);
+        })
+        .for_each(drop);
+    if !extra_values.is_empty() {
+        return Some(extra_values);
+    }
+    return None;
 }
