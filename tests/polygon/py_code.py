4e29596c7f71fcd9988c4dc7ff030e91f085ea04
Mansur Azatbek <mansur62624@gmail.com>
2023-07-04 22:00:09 +0600

    test: check api

--- tests/polygon/py_code.py
+++ tests/polygon/py_code.py
@@ -8,14 +8,20 @@ class ErrorAboba(PystvalException):
     ]
 
 class ErrorNumber(PystvalException):
-    message = "found number {n}"
+    message = "found number {B}"
     rules = [
         Rule("(?P<n>\d+)", MatchRequirement.MustNotBefound)
     ]
 
+class CheckExtraBlank(PystvalException):
+    message = "Extra blank {e}"
+    rules = [
+        Rule("(?P<e>X)", MatchRequirement.MustBeFound)
+    ]
+
 async def init(delay,text):
     await asyncio.sleep(delay)
-    validator = TemplateValidator([ErrorAboba, ErrorNumber])
+    validator = TemplateValidator([ErrorAboba, ErrorNumber, CheckExtraBlank])
     try:
         await validator.async_validate(text)
     except PystvalException as e:
@@ -23,8 +29,8 @@ async def init(delay,text):
 
 async def main():
     tasks = [
-        asyncio.create_task(init(50,b"aboba")),  
-        asyncio.create_task(init(4,b"bob")),
+        asyncio.create_task(init(2,b"aboba 23")),  
+        asyncio.create_task(init(4,b"aboba")),
         asyncio.create_task(init(6,b"HEY")),
     ]
 
