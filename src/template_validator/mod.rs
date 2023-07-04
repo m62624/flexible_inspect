0db33002f8bd13d564318039977dd908a164173b
Mansur Azatbek <mansur62624@gmail.com>
2023-07-04 16:13:38 +0600

    refactor: code divided into files

--- src/template_validator/mod.rs
+++ src/template_validator/mod.rs
@@ -21,3 +21,28 @@ impl TemplateValidator {
         Ok(Self(Arc::new(TemplateSafeSelf::new(py, cartridges)?)))
     }
 }
+
+impl TemplateSafeSelf {
+    pub fn new(py: Python, cartridges: PyObject) -> PyResult<Self> {
+        let mut data = Vec::new();
+        if let Ok(list) = cartridges.downcast::<types::PyList>(py) {
+            list.iter().try_for_each(|class_obj| {
+                if let Ok(class_py) = class_obj.downcast::<types::PyType>() {
+                    data.push(CartridgeWrapper::new(py, class_py.to_object(py))?);
+                    Ok(())
+                } else {
+                    Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
+                        "'{}' must be a 'Class'",
+                        class_obj
+                    )))
+                }
+            })?;
+            Ok(Self { cartridges: data })
+        } else {
+            return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
+                "'{}' must be a 'List[ Class, Class... ]'",
+                cartridges
+            )));
+        }
+    }
+}
--- src/template_validator/validate.rs
+++ src/template_validator/validate.rs
@@ -1,30 +1,5 @@
 use super::*;
 
-impl TemplateSafeSelf {
-    pub fn new(py: Python, cartridges: PyObject) -> PyResult<Self> {
-        let mut data = Vec::new();
-        if let Ok(list) = cartridges.downcast::<types::PyList>(py) {
-            list.iter().try_for_each(|class_obj| {
-                if let Ok(class_py) = class_obj.downcast::<types::PyType>() {
-                    data.push(CartridgeWrapper::new(py, class_py.to_object(py))?);
-                    Ok(())
-                } else {
-                    Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                        "'{}' must be a 'Class'",
-                        class_obj
-                    )))
-                }
-            })?;
-            Ok(Self { cartridges: data })
-        } else {
-            return Err(PyErr::new::<exceptions::PyTypeError, _>(format!(
-                "'{}' must be a 'List[ Class, Class... ]'",
-                cartridges
-            )));
-        }
-    }
-}
-
 #[pymethods]
 impl TemplateValidator {
     pub fn validate(&self, py: Python, text_bytes: &types::PyBytes) -> PyResult<Option<PyObject>> {
