cec77f61d1f1e0aeb5c379188af033f99af1f6da
Mansur Azatbek <mansur62624@gmail.com>
2023-07-05 15:56:47 +0600

    docs: added descriptions

--- pystval.pyi
+++ pystval.pyi
@@ -1,79 +1,131 @@
-"""`Pystval` is a `Rust` library for `Python`, used to create your own validator."""
 import enum
-from typing import Optional, Type, TypeVar, List,Any
+from typing import Optional, Type, TypeVar, List, Any
+
 
 class PystvalErrorMeta(type):
-    def __new__(cls, name: str, bases: tuple[type], attrs: dict[str, any]) -> type:
+    def __new__(cls, name: str, bases: tuple[type], attrs: dict[str, Any]) -> type:
         ...
 
 
 class PystvalException(Exception, metaclass=PystvalErrorMeta):
+    """
+    Custom exception class for Pystval library.
+
+    Attributes
+    ----------
+    `report` : `str`
+        The report message associated with the exception.
+    """
     @property
     def report(self) -> str:
+        """
+        Get the report message associated with the exception.
+
+        Returns
+        -------
+        `str`
+            The report message.
+
+        """
         ...
 
     pass
 
+
 class MatchRequirement(enum.Enum):
     """
-    A `enumeration` that gives options on what to do when you find a regex match 
+    An enumeration that gives options on what to do when you find a regex match 
     """
-    MustBeFound = 0,
+    MustBeFound = 0
     """
     It must be found, otherwise an exception will be thrown
     """
-    MustNotBeFound = 1,
+    MustNotBefound = 1
     """
-    It is not to Be found here, otherwise an exception will be raised
+    It is not to be found here, otherwise an exception will be raised
     """
 
+
 class Rule:
+    """
+    A class for storing a rule with various modifiers and subrules
+    """
+
     def __init__(self, inner: str, requirements: MatchRequirement) -> None:
+        """
+        Parameters
+        ----------
+        `inner` : `str`
+            The inner value for the rule
+        `requirements` : `MatchRequirement`
+            The match requirement for the rule
+
+        Raises
+        ------
+        `TypeError`
+            If the inner parameter is not a string
+
+        """
         ...
-    
-    def extend(self, nested_rules: List[Rule]) -> None:
+
+    def extend(self, nested_rules: List['Rule']) -> None:
+        """
+        Extend the rule with nested rules.
+
+        Parameters
+        ----------
+        `nested_rules` : `List[Rule]`
+            The nested rules to be added
+
+        Raises
+        ------
+        `TypeError`
+            If `nested_rules` is not a list of `Rule` objects
+
+        """
         ...
-    
+
     def counter_is_equal(self, count: int) -> None:
         """
-        Adding a match counter, where the condition: there must be exactly `count` of matches
+        Adding a match counter, where the condition is: there must be exactly `count` matches
         """
         ...
 
     def counter_more_than(self, count: int) -> None:
         """
-      Adding a match counter, where the condition: must be greater than (inclusive) `count` of matches
+        Adding a match counter, where the condition is: there must be greater than or equal to `count` matches
         """
         ...
-# 
+
     def counter_less_than(self, count: int) -> None:
         """
-        Adding a match counter, where the condition: must be less than (inclusive) `count` of matches
+        Adding a match counter, where the condition is: there must be less than or equal to `count` matches
         """
         ...
-   
+
 
 class TemplateValidator:
     """
     A class for creating a validator
     """
 
-    def __init__(self, flags: List[Any]):
+    def __init__(self, flags: List[Type[Any]]) -> None:
         """
         Parameters
         ----------
-        `flags` : `List[Any]`
+        `flags` : `List[Type[Any]]`
             List of classes
 
         Raises
         ------
         `TypeError`
-            Specifies a different data type
+            If a different data type is specified
         `AttributeError`
-            Incorrect attribute specified (missing)
+            If an incorrect attribute is specified or missing
         """
+        ...
 
-    async def async_validate(self, text: bytes):
+    async def async_validate(self, text: bytes) -> None:
         """
         Parameters
         ----------
@@ -82,11 +134,10 @@ class TemplateValidator:
 
         Raises
         ------
-        `Custom Error (PystvalError)`
-            The error that was specified in `flags`
+        `CustomError(PystvalException)`
+           Custom error (`PystvalException`), which were specified in `flags`.
         """
-        pass
-    pass
+        ...
 
     def validate(self, text: bytes) -> Optional[List[PystvalException]]:
         """
@@ -97,10 +148,7 @@ class TemplateValidator:
 
         Raises
         ------
-        `Custom Error (PystvalError)`
-            The error that was specified in `flags`
+        `List[CustomError(PystvalException)]`
+            List of custom errors (`PystvalException`) that were specified in `flags`
         """
-        pass
-    pass
-
-
+        ...
