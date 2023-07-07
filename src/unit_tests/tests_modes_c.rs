f6b12f0255810c28514647c92a1e92a6baa16608
Mansur Azatbek <mansur62624@gmail.com>
2023-07-08 00:50:15 +0600

    test: added unit test

--- src/unit_tests/tests_modes_c.rs
+++ src/unit_tests/tests_modes_c.rs
@@ -12,26 +12,27 @@ mod mode_all_rules_for_all_matches {
             let class_error = mock_obj::make_obj(
                 py,
                 "custom error with value {number}",
-                Some(vec![Rule::spawn(r"\[.+\]", MatchRequirement::MustBeFound)?
-                    .extend_t(
-                        py,
-                        vec![
-                            Rule::spawn(r"(?P<subrule>\[.+\])", MatchRequirement::MustBeFound)?
-                                .extend_t(
-                                    py,
-                                    vec![
-                                        Rule::spawn(
-                                            r"(?P<number>\d+)",
-                                            MatchRequirement::MustBeFound,
-                                        )?,
-                                        Rule::spawn(r"BOBA", MatchRequirement::MustNotBefound)?,
-                                    ],
-                                )?,
-                            Rule::spawn(r"ABOBA", MatchRequirement::MustNotBefound)?,
-                            Rule::spawn(r".*", MatchRequirement::MustBeFound)?,
-                            Rule::spawn(r".+", MatchRequirement::MustBeFound)?,
-                        ],
-                    )?]),
+                Some(vec![Rule::spawn(
+                    r"\[[^\]]+\]
+                ",
+                    MatchRequirement::MustBeFound,
+                )?
+                .extend_t(
+                    py,
+                    vec![
+                        Rule::spawn(r"(?P<subrule>\[.+\])", MatchRequirement::MustBeFound)?
+                            .extend_t(
+                                py,
+                                vec![
+                                    Rule::spawn(r"(?P<number>\d+)", MatchRequirement::MustBeFound)?,
+                                    Rule::spawn(r"BOBA", MatchRequirement::MustNotBefound)?,
+                                ],
+                            )?,
+                        Rule::spawn(r"ABOBA", MatchRequirement::MustNotBefound)?,
+                        Rule::spawn(r".*", MatchRequirement::MustBeFound)?,
+                        Rule::spawn(r".+", MatchRequirement::MustBeFound)?,
+                    ],
+                )?]),
             );
             let validator =
                 TemplateValidator::new(py, types::PyList::new(py, [class_error]).into_py(py))?;
@@ -51,30 +52,25 @@ mod mode_all_rules_for_at_least_one_match {
     fn runner_t_0() -> PyResult<()> {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| -> PyResult<()> {
-            let text = "[[AL_XL_191_KJ_OL]]";
+            let text = "BOBA [[AL_XL_191_KJ_OL]] QLWOE [ABOBA] [BOBA]";
             let class_error = mock_obj::make_obj(
                 py,
                 "custom error with value {number}",
-                Some(vec![Rule::spawn(r"\[.+\]", MatchRequirement::MustBeFound)?
-                    .extend_t(
-                        py,
-                        vec![
-                            Rule::spawn(r"(?P<subrule>\[.+\])", MatchRequirement::MustBeFound)?
-                                .extend_t(
-                                    py,
-                                    vec![
-                                        Rule::spawn(
-                                            r"(?P<number>\d+)",
-                                            MatchRequirement::MustBeFound,
-                                        )?,
-                                        Rule::spawn(r"BOBA", MatchRequirement::MustNotBefound)?,
-                                    ],
-                                )?,
-                            Rule::spawn(r"ABOBA", MatchRequirement::MustNotBefound)?,
-                            Rule::spawn(r".*", MatchRequirement::MustBeFound)?,
-                            Rule::spawn(r".+", MatchRequirement::MustBeFound)?,
-                        ],
-                    )?]),
+                Some(vec![Rule::spawn(
+                    r"\[[^\]]+\]",
+                    MatchRequirement::MustBeFound,
+                )?
+                .extend_t(
+                    py,
+                    vec![
+                        Rule::spawn(r"(?P<subrule>\[.+\])", MatchRequirement::MustBeFound)?,
+                        Rule::spawn(r"ABOBA", MatchRequirement::MustNotBefound)?,
+                        Rule::spawn(r"\d+", MatchRequirement::MustNotBefound)?,
+                        Rule::spawn(r".*", MatchRequirement::MustBeFound)?,
+                        Rule::spawn(r".+", MatchRequirement::MustBeFound)?,
+                    ],
+                )?
+                .mode_all_rules_for_at_least_one_match()]),
             );
             let validator =
                 TemplateValidator::new(py, types::PyList::new(py, [class_error]).into_py(py))?;
