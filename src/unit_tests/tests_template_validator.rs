3225454755b3ecc50355401c38212a345ef2b14e
Mansur Azatbek <mansur62624@gmail.com>
2023-07-10 01:21:07 +0600

    test: added unit test

--- src/unit_tests/tests_template_validator.rs
+++ src/unit_tests/tests_template_validator.rs
@@ -14,10 +14,56 @@ mod fn_new {
                 py,
                 types::PyList::new(py, [class_py]).into_py(py),
             )?;
+            dbg!(&template_val);
+            Ok(())
+        })
+    }
+}
+mod fn_validate {
+    use super::*;
+    #[test]
+    fn validate_t_0() -> PyResult<()> {
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| -> PyResult<()> {
+            let rule1 = Rule::spawn(r"(?P<number>\d+)", MatchRequirement::MustNotBeFound)?;
+            let class_py = mock_obj::make_obj(py, "test", Some(vec![rule1]));
+
+            let template_val = template_validator::TemplateValidator::new(
+                py,
+                types::PyList::new(py, [class_py]).into_py(py),
+            )?;
             let text_bytes = types::PyBytes::new(py, "test 123".as_bytes());
             dbg!(&template_val);
-            template_val.validate(py, text_bytes)?;
+            assert_eq!(template_val.validate(py, text_bytes)?.is_some(), true);
             Ok(())
+        })
+    }
+}
+
+mod fn_async_validate {
+    use super::*;
+    use crate::{cartridge::CartridgeWrapper, rule::next::NextStep};
+
+    #[test]
+    fn async_validate_t_0() -> PyResult<()> {
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| -> PyResult<()> {
+            let rule1 = Rule::spawn(r"(?P<number>\d+)", MatchRequirement::MustNotBeFound)?;
+            let class_py = mock_obj::make_obj(py, "test", Some(vec![rule1]));
+            let text = "test 123".to_string();
+
+            async_std::task::block_on(async {
+                assert_eq!(
+                    matches!(
+                        CartridgeWrapper::new(py, class_py)?
+                            .async_run(Arc::new(text))
+                            .await,
+                        NextStep::Error(_)
+                    ),
+                    true
+                );
+                Ok(())
+            })
         })
     }
 }
