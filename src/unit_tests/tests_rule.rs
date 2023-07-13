c7b18ec125e76a91becceebac73c2a425b966d01
Mansur Azatbek <mansur62624@gmail.com>
2023-07-13 13:17:38 +0600

    test: added unit tests

--- src/unit_tests/tests_rule.rs
+++ src/unit_tests/tests_rule.rs
@@ -422,3 +422,53 @@ mod fn_run {
         }
     }
 }
+
+mod fn_auto_generate_rule {
+    use super::*;
+    #[test]
+    fn auto_generate_t_0() -> PyResult<()> {
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| -> PyResult<()> {
+            let texts = types::PyList::new(
+                py,
+                ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+            );
+            assert_eq!(
+                Rule::auto_generate(MatchRequirement::MustBeFound, texts)?.as_ref(),
+                "^(?:1[01]|1|[2-9])$"
+            );
+
+            Ok(())
+        })
+    }
+
+    #[test]
+    fn auto_generate_t_1() -> PyResult<()> {
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| -> PyResult<()> {
+            let texts = types::PyList::new(py, ["bob", "aboba", "b", "ba"]);
+            assert_eq!(
+                Rule::auto_generate(MatchRequirement::MustBeFound, texts)?.as_ref(),
+                "^(?:aboba|b(?:(?:ob|a))?)$"
+            );
+            Ok(())
+        })
+    }
+
+    #[test]
+    fn auto_generate_t_2() -> PyResult<()> {
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| -> PyResult<()> {
+            let texts = types::PyList::new(
+                py,
+                ["
+            "],
+            );
+            assert_eq!(
+                Rule::auto_generate(MatchRequirement::MustBeFound, texts)?.as_ref(),
+                "^(?:aboba|b(?:(?:ob|a))?)$"
+            );
+            Ok(())
+        })
+    }
+}
