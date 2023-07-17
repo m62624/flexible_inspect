05739a84dbc25a5f37e52341b8b84694a2f22d18
Mansur Azatbek <mansur62624@gmail.com>
2023-07-17 22:30:24 +0600

    fix: mode `all_rules_for_at_least_one_match`

--- src/rule/runner/context_match/mod.rs
+++ src/rule/runner/context_match/mod.rs
@@ -1,2 +1,3 @@
-pub mod all_rules_for_all_matches;
-pub mod at_least_one_rule_for_all_matches;
+mod all_rules_for_all_matches;
+mod all_rules_for_at_least_one_match;
+mod at_least_one_rule_for_all_matches;
