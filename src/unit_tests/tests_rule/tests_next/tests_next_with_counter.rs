9c858f10716fe8c55e302ccd3f0348d5c3e62cc8
Mansur Azatbek <mansur62624@gmail.com>
2023-07-18 22:22:19 +0600

    tests: added tests

--- src/unit_tests/tests_rule/tests_next/tests_next_with_counter.rs
+++ src/unit_tests/tests_rule/tests_next/tests_next_with_counter.rs
@@ -1,93 +1,106 @@
 use super::*;
 use crate::{captures::CaptureData, rule::next::NextStep};
-mod tests_counter;
-mod tests_next_with_counter;
 
 /// Проверка next_or_error, где requirement - MustBeFound, Captures - TRUE, Subrules - TRUE,
+/// + counter
 #[test]
 pub fn next_or_error_t_0() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
-        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.extend_t(
-            py,
-            vec![Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?],
-        )?;
+        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?
+            .counter_is_equal(2)
+            .extend_t(
+                py,
+                vec![Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.counter_is_equal(2)],
+            )?;
         let mut captures = CaptureData::find_captures(&rule, "lock");
+        let err_data = captures.hashmap_for_error.clone();
         assert_eq!(
-            Rule::next_or_data_for_error(&rule, &mut captures,),
-            NextStep::Go
+            Rule::next_or_data_for_error(&rule, &mut captures),
+            NextStep::Error(Some(err_data)),
         );
         Ok(())
     })
 }
 
 /// Проверка next_or_error, где requirement - MustBeFound, Captures - TRUE, Subrules - FALSE,
+/// + counter
 #[test]
 pub fn next_or_error_t_1() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
-    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?;
+    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.counter_is_equal(2);
     let mut captures = CaptureData::find_captures(&rule, "lock");
+    let err_data = captures.hashmap_for_error.clone();
     assert_eq!(
-        Rule::next_or_data_for_error(&rule, &mut captures,),
-        NextStep::Finish
+        Rule::next_or_data_for_error(&rule, &mut captures),
+        NextStep::Error(Some(err_data)),
     );
     Ok(())
 }
 
 /// Проверка next_or_error, где requirement - MustBeFound, Captures - FALSE, Subrules - TRUE
+/// + counter
 #[test]
 pub fn next_or_error_t_3() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
-        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.extend_t(
-            py,
-            vec![Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?],
-        )?;
+        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?
+            .counter_is_equal(2)
+            .extend_t(
+                py,
+                vec![Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.counter_is_equal(2)],
+            )?;
         let mut captures = CaptureData::find_captures(&rule, "");
         assert_eq!(
-            Rule::next_or_data_for_error(&rule, &mut captures,),
-            NextStep::Error(None)
+            Rule::next_or_data_for_error(&rule, &mut captures),
+            NextStep::Error(None),
         );
         Ok(())
     })
 }
 
 /// Проверка next_or_error, где requirement - MustBeFound, Captures - FALSE, Subrules - FALSE
+/// + counter
 #[test]
 pub fn next_or_error_t_4() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
-    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?;
+    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustBeFound)?.counter_is_equal(2);
     let mut captures = CaptureData::find_captures(&rule, "");
     assert_eq!(
-        Rule::next_or_data_for_error(&rule, &mut captures,),
-        NextStep::Error(None)
+        Rule::next_or_data_for_error(&rule, &mut captures),
+        NextStep::Error(None),
     );
     Ok(())
 }
 
 /// Проверка next_or_error, где requirement - MustNotBeFound, Captures - TRUE, Subrules - TRUE,
+/// + counter
 #[test]
 pub fn next_or_error_t_5() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
-        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.extend_t(
-            py,
-            vec![Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?],
-        )?;
+        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?
+            .counter_is_equal(2)
+            .extend_t(
+                py,
+                vec![Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.counter_is_equal(2)],
+            )?;
         let mut captures = CaptureData::find_captures(&rule, "lock");
+        let err_data = captures.hashmap_for_error.clone();
         assert_eq!(
-            Rule::next_or_data_for_error(&rule, &mut captures,),
-            NextStep::Go
+            Rule::next_or_data_for_error(&rule, &mut captures),
+            NextStep::Error(Some(err_data)),
         );
         Ok(())
     })
 }
 
 /// Проверка next_or_error, где requirement - MustNotBeFound, Captures - TRUE, Subrules - FALSE,
+/// + counter
 #[test]
 pub fn next_or_error_t_6() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
-    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?;
+    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.counter_is_equal(2);
     let mut captures = CaptureData::find_captures(&rule, "lock");
     let err_data = captures.hashmap_for_error.clone();
     assert_eq!(
@@ -98,32 +111,38 @@ pub fn next_or_error_t_6() -> PyResult<()> {
 }
 
 /// Проверка next_or_error, где requirement - MustNotBeFound, Captures - FALSE, Subrules - TRUE
+/// + counter
 #[test]
 pub fn next_or_error_t_7() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
-        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.extend_t(
-            py,
-            vec![Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?],
-        )?;
+        let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?
+            .counter_is_equal(2)
+            .extend_t(
+                py,
+                vec![Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.counter_is_equal(2)],
+            )?;
         let mut captures = CaptureData::find_captures(&rule, "");
+        let err_data = captures.hashmap_for_error.clone();
         assert_eq!(
             Rule::next_or_data_for_error(&rule, &mut captures),
-            NextStep::Finish,
+            NextStep::Error(Some(err_data)),
         );
         Ok(())
     })
 }
 
 /// Проверка next_or_error, где requirement - MustNotBeFound, Captures - FALSE, Subrules - FALSE
+/// + counter
 #[test]
 pub fn next_or_error_t_8() -> PyResult<()> {
     pyo3::prepare_freethreaded_python();
-    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?;
+    let rule = Rule::spawn(r"(\w+)", MatchRequirement::MustNotBeFound)?.counter_is_equal(2);
     let mut captures = CaptureData::find_captures(&rule, "");
+    let err_data = captures.hashmap_for_error.clone();
     assert_eq!(
-        Rule::next_or_data_for_error(&rule, &mut captures,),
-        NextStep::Finish
+        Rule::next_or_data_for_error(&rule, &mut captures),
+        NextStep::Error(Some(err_data)),
     );
     Ok(())
 }
