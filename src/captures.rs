97612538588d03ca584af631dce3c6ae4b47f949
Mansur Azatbek <mansur62624@gmail.com>
2023-07-18 14:44:22 +0600

    feat: added a new type for duplicates

--- src/captures.rs
+++ src/captures.rs
@@ -1,13 +1,20 @@
 use super::rule::{RegexRaw, Rule};
 use std::collections::{HashMap, HashSet};
 
+/// Структура для хранения типа захвата, с дублированием данных или без
+#[derive(Debug)]
+pub enum CaptureType<'s> {
+    Single(HashSet<&'s str>),
+    Multiple(Vec<&'s str>),
+}
+
 /// Структура для хранения совпадений
 #[derive(Debug)]
 pub struct CaptureData<'s> {
     /// Хранит совпадения по именам, для заполнения `extra` в сообщений ошибки
     pub hashmap_for_error: HashMap<String, String>,
     // Хранит совпадения по тексту, для проверок подправил
-    pub text_for_capture: HashSet<&'s str>,
+    pub text_for_capture: CaptureType<'s>,
     // Хранит количество совпадений, для проверки `Counter`
     pub counter_value: usize,
 }
@@ -17,10 +24,12 @@ impl<'s> CaptureData<'s> {
     pub fn find_captures(rule: &Rule, text: &'s str) -> Self {
         let mut hashmap_for_error = HashMap::new();
         let mut text_for_capture = HashSet::new();
+        let mut text_for_capture_duplicate = Vec::new();
         let mut counter: usize = 0;
         // флаг для проверки `Counter`
         let flag_check_counter = rule.content_unchecked().counter.is_some();
         // На первый взгляд мы видим дублирование кода, но каждый `match` работает с разными структурами
+
         match &rule.content_unchecked().str_with_type {
             RegexRaw::DefaultR(pattern) => {
                 // создаем регулярное выражение
@@ -31,7 +40,11 @@ impl<'s> CaptureData<'s> {
                         hashmap_for_error
                             .entry("main_capture".into())
                             .or_insert_with(|| value.as_str().into());
-                        text_for_capture.insert(value.as_str());
+                        if rule.content_unchecked().duplicate_matches {
+                            text_for_capture_duplicate.push(value.as_str());
+                        } else {
+                            text_for_capture.insert(value.as_str());
+                        }
                         // в одном `regex` может быть несколько групп, но все
                         // они нужны для получения главного совпадения, потому
                         // повышение идет только в `main capture`
@@ -61,7 +74,11 @@ impl<'s> CaptureData<'s> {
                             hashmap_for_error
                                 .entry("main_capture".into())
                                 .or_insert_with(|| value.as_str().into());
-                            text_for_capture.insert(value.as_str());
+                            if rule.content_unchecked().duplicate_matches {
+                                text_for_capture_duplicate.push(value.as_str());
+                            } else {
+                                text_for_capture.insert(value.as_str());
+                            }
                             // в одном `regex` может быть несколько групп, но все
                             // они нужны для получения главного совпадения, потому
                             // повышение идет только в `main capture`
@@ -86,12 +103,19 @@ impl<'s> CaptureData<'s> {
         // возвращаем структуру
         Self {
             hashmap_for_error,
-            text_for_capture,
+            text_for_capture: if rule.content_unchecked().duplicate_matches {
+                CaptureType::Multiple(text_for_capture_duplicate)
+            } else {
+                CaptureType::Single(text_for_capture)
+            },
             counter_value: counter,
         }
     }
     /// Проверка присутствия совпадений
     pub fn is_some(&self) -> bool {
-        !self.text_for_capture.is_empty()
+        match &self.text_for_capture {
+            CaptureType::Single(s) => !s.is_empty(),
+            CaptureType::Multiple(v) => !v.is_empty(),
+        }
     }
 }
