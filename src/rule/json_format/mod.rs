c6691edf7890e8e237d12a0ea82920b74c0a0493
Mansur Azatbek <mansur62624@gmail.com>
2023-07-20 15:36:05 +0600

    refactor: changed string to &str

--- src/rule/json_format/mod.rs
+++ src/rule/json_format/mod.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 impl Serialize for SerializableRegexSet {
+    /// Сериализует `RegexSet` в `Vec<String>`
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -11,11 +12,12 @@ impl Serialize for SerializableRegexSet {
 }
 
 impl<'de> Deserialize<'de> for SerializableRegexSet {
+    /// Десериализует `Vec<&str>` в `RegexSet`
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let patterns: Vec<String> = Deserialize::deserialize(deserializer)?;
+        let patterns: Vec<&str> = Deserialize::deserialize(deserializer)?;
         let regex_set = regex::RegexSet::new(&patterns).map_err(DeError::custom)?;
         Ok(Self { regex_set })
     }
