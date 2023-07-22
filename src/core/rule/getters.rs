23bf97d354f1f6f742dfefbc75ac29ffb1244a65
Mansur Azatbek <mansur62624@gmail.com>
2023-07-23 00:58:43 +0600

    docs: updated documentation

--- src/core/rule/getters.rs
+++ src/core/rule/getters.rs
@@ -1,20 +1,17 @@
 use super::*;
 
 impl Rule {
-    /// Получем содержимое из правило, без типа PyResult, так как пользователю
-    /// необходимо знать, что правило потеряно из за модификатора,
-    /// так как модификатор берет себе правило как владелец и возращает новое
-    /// на основе него, с другими модфикаторами или же подправилами
+    /// Use for direct access to the structure body
     pub(crate) fn content_unchecked(&self) -> &TakeRuleForExtend {
         self.0.as_ref().expect(ERR_OPTION)
     }
 
-    /// То же самое что и `content_unchecked`, только для изменения содержимого
+    /// Use for direct access and modification to the body of the structure
     pub(crate) fn content_mut_unchecked(&mut self) -> &mut TakeRuleForExtend {
         self.0.as_mut().expect(ERR_OPTION)
     }
 
-    /// Получаем отобранные правила из `RegexSet`
+    /// Get selected rules from `RegexSet`
     pub(crate) fn get_selected_rules(regex_set: &regex::RegexSet, text: &str) -> Vec<usize> {
         regex_set.matches(text).iter().collect()
     }
