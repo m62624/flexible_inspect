42ff8489afbc395ceabd8070963928ce6e2169ae
Mansur Azatbek <mansur62624@gmail.com>
2023-07-22 22:55:30 +0600

    tests: added tests

--- src/unit_tests/core/hasher.rs
+++ src/unit_tests/core/hasher.rs
@@ -1,2 +1,52 @@
 use super::*;
+use std::collections::hash_map;
+use std::hash::Hash;
+use std::hash::Hasher;
 
+mod fn_new {
+    use super::*;
+
+    /// Проверяем, что хеши одинаковые при одинаковых параметрах
+    #[test]
+    fn new_hash_t_0() {
+        let mut hasher_1 = hash_map::DefaultHasher::new();
+        let rule_1 = Rule::new(r"\s", MatchRequirement::MustBeFound);
+        rule_1.hash(&mut hasher_1);
+        let hasher_1_r = hasher_1.finish();
+
+        let mut hasher_2 = hash_map::DefaultHasher::new();
+        let rule_2 = Rule::new(r"\s", MatchRequirement::MustBeFound);
+        rule_2.hash(&mut hasher_2);
+        let hasher_2_r = hasher_2.finish();
+        assert_eq!(hasher_1_r, hasher_2_r);
+    }
+
+    /// Проверяем, что хеши разные при разных параметрах
+    #[test]
+    fn new_hash_t_1() {
+        let mut hasher_1 = hash_map::DefaultHasher::new();
+        let rule_1 = Rule::new(r"\w", MatchRequirement::MustBeFound);
+        rule_1.hash(&mut hasher_1);
+        let hasher_1_r = hasher_1.finish();
+
+        let mut hasher_2 = hash_map::DefaultHasher::new();
+        let rule_2 = Rule::new(r"\s", MatchRequirement::MustBeFound);
+        rule_2.hash(&mut hasher_2);
+        let hasher_2_r = hasher_2.finish();
+        assert_ne!(hasher_1_r, hasher_2_r);
+    }
+
+    #[test]
+    fn new_hash_t_2() {
+        let mut hasher_1 = hash_map::DefaultHasher::new();
+        let rule_1 = Rule::new(r"\s", MatchRequirement::MustBeFound);
+        rule_1.hash(&mut hasher_1);
+        let hasher_1_r = hasher_1.finish();
+
+        let mut hasher_2 = hash_map::DefaultHasher::new();
+        let rule_2 = Rule::new(r"\s", MatchRequirement::MustBeFound);
+        rule_2.hash(&mut hasher_2);
+        let hasher_2_r = hasher_2.finish();
+        assert_eq!(hasher_1_r, hasher_2_r);
+    }
+}
--- src/unit_tests/core/rule/mod.rs
+++ src/unit_tests/core/rule/mod.rs
@@ -3,11 +3,26 @@ use super::*;
 /// Проверка конструктора `Rule`
 mod fn_new {
     use super::*;
+    use crate::core::rule::RegexRaw;
 
-    /// Создаем правило с помощью конструктора `Regex` (MatchRequirement::MustBeFound)
+    /// Должны определить, что это `DefaultRegex`
     #[test]
     fn new_t_0() {
-        Rule::new(r"(?-u)(?<cstr>[^\x00]+)\x00", MatchRequirement::MustBeFound);
+        let rule = Rule::new(r"\d", MatchRequirement::MustBeFound);
+        assert_eq!(
+            &rule.content_unchecked().str_with_type,
+            &RegexRaw::DefaultRegex(String::from(r"\d").into_boxed_str())
+        );
+    }
+
+    /// Должны определить, что это `FancyRegex`
+    #[test]
+    fn new_t_1() {
+        let rule = Rule::new(r"\d(?=\d)", MatchRequirement::MustBeFound);
+        assert_eq!(
+            &rule.content_unchecked().str_with_type,
+            &RegexRaw::FancyRegex(String::from(r"\d(?=\d)").into_boxed_str())
+        );
     }
 
     #[test]
