84cd5e7ab6d97ce49567c1bff0976b7be523fd66
Mansur Azatbek <mansur62624@gmail.com>
2023-07-24 00:47:58 +0600

    refactor: changed visibility `DEFAULT_CAPTURE`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,4 +2,4 @@ pub mod rules;
 
 const ERR_OPTION: &str = " The body of `Rule` is missing, maybe you used modifiers, they borrow `Rule`, modifiers modify it and return the already modified version";
 
-const DEFAULT_CAPTURE: &str = "main_capture";
+pub const DEFAULT_CAPTURE: &str = "main_capture";
