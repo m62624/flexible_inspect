6ab75ac19a9cc698363f441518c4640206509cec
Mansur Azatbek <mansur62624@gmail.com>
2023-07-23 14:30:11 +0600

    refactor(`Rule`): added restriction on adding certain type of rules

--- src/core/rule/slice.rs
+++ src/core/rule/slice.rs
@@ -2,7 +2,10 @@ use super::*;
 
 impl SlisedRules {
     /// The method for sorting all nested rules
-    pub fn slice_rules<T: IntoIterator<Item = Rule>>(all_rules: T) -> SlisedRules {
+    pub fn slice_rules<T: IntoIterator<Item = Rule>>(
+        root_rule: &Rule,
+        all_rules: T,
+    ) -> SlisedRules {
         /*
         We do not save directly via `insert` to `IndexSet` as we would lose the ordering,
         we need the order to properly check rules that are not in `RegexSet` when searching through the index.
@@ -13,13 +16,59 @@ impl SlisedRules {
         let mut o_complex_rules = Vec::new();
         let mut o_bytes_rules = Vec::new();
 
+        let mut rule_access = |root_type: &Rule, rule: Rule| {
+            // Common part of the panic message
+            let panic_msg = format!(
+"\n\n\nonly regular expressions for `&str` strings can be added to a regular expression for `&str` strings, not for &[u8] bytes.
+=============================
+Root rule \"( {:?},{:#?} )\"
+=============================
+Problem rule \"( {:?},{:#?} )\"
+=============================
+If you want to add a regular expression for &[u8] bytes, then use the `for_bytes` method.\n\n\n",
+            root_type.content_unchecked().str_with_type,
+            root_rule.content_unchecked().mutable_modifiers.requirement, 
+            rule.content_unchecked().str_with_type,
+            rule.content_unchecked().mutable_modifiers.requirement
+        );
+            match root_type.content_unchecked().str_with_type {
+                RegexRaw::DefaultRegex(_) => {
+                    if let RegexRaw::RegexBytes(_) = rule.content_unchecked().str_with_type {
+                        panic!("{}", panic_msg)
+                    }
+                    o_simple_rules.push(rule);
+                }
+                RegexRaw::FancyRegex(_) => {
+                    if let RegexRaw::RegexBytes(_) = rule.content_unchecked().str_with_type {
+                        panic!("{}", panic_msg)
+                    }
+                    o_complex_rules.push(rule);
+                }
+                RegexRaw::RegexBytes(_) => {
+                    if let RegexRaw::DefaultRegex(_) | RegexRaw::FancyRegex(_) =
+                        rule.content_unchecked().str_with_type
+                    {
+                        panic!(
+"\n\n\nonly regular expressions for &[u8] bytes can be added to a regular expression for &[u8] bytes, not for `&str` strings.
+=============================
+Root rule \"( {:?},{:#?} )\"
+=============================
+Problem rule \"( {:?},{:#?} )\"
+=============================\n\n\n",
+            root_type.content_unchecked().str_with_type,
+            root_rule.content_unchecked().mutable_modifiers.requirement, 
+            rule.content_unchecked().str_with_type,
+            rule.content_unchecked().mutable_modifiers.requirement
+)
+                    }
+                    o_bytes_rules.push(rule);
+                }
+            }
+        };
+
         all_rules
             .into_iter()
-            .for_each(|rule| match rule.content_unchecked().str_with_type {
-                RegexRaw::DefaultRegex(_) => o_simple_rules.push(rule),
-                RegexRaw::FancyRegex(_) => o_complex_rules.push(rule),
-                RegexRaw::RegexBytes(_) => o_bytes_rules.push(rule),
-            });
+            .for_each(|rule| rule_access(&root_rule, rule));
 
         let o_simple_rules: IndexSet<_> = o_simple_rules.into_iter().collect();
         let o_bytes_rules: IndexSet<_> = o_bytes_rules.into_iter().collect();
