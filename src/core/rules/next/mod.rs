e50c4f568cf91425dcaad8e0a019647f200dd122
Mansur Azatbek <mansur62624@gmail.com>
2023-07-24 15:04:52 +0600

    chore: temporary template `next`

--- src/core/rules/next/mod.rs
+++ src/core/rules/next/mod.rs
@@ -1,3 +1,5 @@
+use super::{captures::CaptureData, traits::RuleBase};
+use crate::MatchRequirement;
 use std::collections::HashMap;
 
 /// All optional modifiers return `NextStep` to have a unified type
@@ -6,4 +8,28 @@ pub enum NextStep {
     Go,
     Finish,
     Error(Option<HashMap<String, String>>),
+}
+
+impl NextStep {
+    pub fn next_or_finish_or_error<T: RuleBase>(rule: T, captures: CaptureData) -> NextStep {
+        match rule.get_requirement() {
+            MatchRequirement::MustBeFound => {
+                match (captures.is_some(), rule.get_subrules().is_some()) {
+                    (true, true) => todo!(),
+                    (true, false) => todo!(),
+                    (false, true) => todo!(),
+                    (false, false) => todo!(),
+                }
+            }
+            MatchRequirement::MustNotBeFound => {
+                match (captures.is_some(), rule.get_subrules().is_some()) {
+                    (true, true) => todo!(),
+                    (true, false) => todo!(),
+                    (false, true) => todo!(),
+                    (false, false) => todo!(),
+                }
+            }
+        }
+        todo!()
+    }
 }
