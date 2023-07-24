e2d81b8b98d164a89b32a4112fe7d692d8ac0f08
Mansur Azatbek <mansur62624@gmail.com>
2023-07-24 10:53:06 +0600

    tests: changed tests

--- src/unit_tests/tests_rules/tests_rule_str/tests_captures.rs
+++ src/unit_tests/tests_rules/tests_rule_str/tests_captures.rs
@@ -8,7 +8,7 @@ use super::*;
 #[test]
 fn find_captures_t_0() {
     let text = "1 2 3 4 567";
-    let rule = Rule::new(r"\d+", MatchRequirement::MustBeFound);
+    let rule = Rule::new(r"(?P<aboba>\d+)", MatchRequirement::MustBeFound);
     let captures_1 = Rule::find_captures(&rule, text);
     let captures_2 = HashSet::from(["1", "2", "3", "4", "567"]);
     assert!(captures_1.text_for_capture.is_subset(&captures_2));
@@ -22,7 +22,7 @@ fn find_captures_t_0() {
 #[test]
 fn find_captures_t_1() {
     let text = "1 2 3 4 567 123";
-    let rule = Rule::new(r"\d+(?=\d)", MatchRequirement::MustBeFound);
+    let rule = Rule::new(r"(?P<aboba>\d+(?=\d))", MatchRequirement::MustBeFound);
     let captures_1 = Rule::find_captures(&rule, text);
     let captures_2 = HashSet::from(["12", "56"]);
     assert!(captures_1.text_for_capture.is_subset(&captures_2));
