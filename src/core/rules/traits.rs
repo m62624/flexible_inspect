19c2dd6aaee228f6f764760ea2377e4012f9e616
Mansur Azatbek <mansur62624@gmail.com>
2023-07-24 11:02:36 +0600

    docs: updated documentation

--- src/core/rules/traits.rs
+++ src/core/rules/traits.rs
@@ -11,13 +11,21 @@ pub trait RuleBase {
 
 /// This trait requires method implementations that are different for different structures
 pub trait RuleExtendBase<'s>: RuleBase {
+    /// This method returns the indices of the selected rules that will exactly trigger based on the data ( `&str` | `&[u8]` )
     fn get_selected_rules(regex_set: &regex::RegexSet, text: &str) -> Vec<usize>;
+
+    /// This method finds matches on a regular expression.
+    /// Saves one first match for each group to fill in the error message, and all matches to pass on to the next rule check
     fn find_captures(rule: &Rule, text: &'s str) -> CaptureDataStr<'s>;
 }
 
 /// This trait requires method implementations that are different for different structures
 pub trait RuleBytesExtendBase<'s>: RuleBase {
+    /// This method returns the indices of the selected rules that will exactly trigger based on the data ( `&str` | `&[u8]` )
     fn get_selected_rules(regex_set: &regex::bytes::RegexSet, text_bytes: &[u8]) -> Vec<usize>;
+
+    /// This method finds matches on a regular expression.
+    /// Saves one first match for each group to fill in the error message, and all matches to pass on to the next rule check
     fn find_captures(rule: &RuleBytes, text_bytes: &'s [u8]) -> CaptureDataBytes<'s>;
 }
 
@@ -25,6 +33,7 @@ pub trait RuleBytesExtendBase<'s>: RuleBase {
 pub trait RuleModifiers {
     type RuleType;
 
+    /// a method for extending the rule with nested rules
     fn extend<T: IntoIterator<Item = Self::RuleType>>(&mut self, nested_rules: T)
         -> Self::RuleType;
 }
