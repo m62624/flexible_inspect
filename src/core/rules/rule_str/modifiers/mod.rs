748c2b3ad0cd1d522f0b8331de648e63bccb606a
Mansur Azatbek <mansur62624@gmail.com>
2023-07-24 20:32:00 +0600

    feat: added `counter` methods

--- src/core/rules/rule_str/modifiers/mod.rs
+++ src/core/rules/rule_str/modifiers/mod.rs
@@ -1,6 +1,6 @@
 mod extend;
 use super::*;
-use crate::core::rules::traits::RuleModifiers;
+use crate::core::rules::traits::{RuleBase, RuleModifiers};
 
 impl RuleModifiers for Rule {
     type RuleType = Rule;
@@ -10,5 +10,20 @@ impl RuleModifiers for Rule {
         nested_rules: T,
     ) -> Self::RuleType {
         extend::extend(self, nested_rules)
+    }
+
+    fn counter_is_equal(&mut self, count: usize) -> Self::RuleType {
+        self.content_mut_unchecked().general_modifiers.counter = Some(Counter::Only(count));
+        std::mem::take(self)
+    }
+
+    fn counter_more_than(&mut self, count: usize) -> Self::RuleType {
+        self.content_mut_unchecked().general_modifiers.counter = Some(Counter::MoreThan(count));
+        std::mem::take(self)
+    }
+
+    fn counter_less_than(&mut self, count: usize) -> Self::RuleType {
+        self.content_mut_unchecked().general_modifiers.counter = Some(Counter::LessThan(count));
+        std::mem::take(self)
     }
 }
--- src/core/rules/traits.rs
+++ src/core/rules/traits.rs
@@ -39,4 +39,8 @@ pub trait RuleModifiers {
     /// a method for extending the rule with nested rules
     fn extend<T: IntoIterator<Item = Self::RuleType>>(&mut self, nested_rules: T)
         -> Self::RuleType;
+
+    fn counter_is_equal(&mut self, count: usize) -> Self::RuleType;
+    fn counter_more_than(&mut self, count: usize) -> Self::RuleType;
+    fn counter_less_than(&mut self, count: usize) -> Self::RuleType;
 }
