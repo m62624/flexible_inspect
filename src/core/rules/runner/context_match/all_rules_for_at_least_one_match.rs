5e1b03d2a093159229401c23cc87e9bc83244814
Mansur Azatbek <mansur62624@gmail.com>
2023-07-27 00:21:12 +0600

    chore: temporary template `all_rules_for_at_least_one_match` (again this error)

--- src/core/rules/runner/context_match/all_rules_for_at_least_one_match.rs
+++ src/core/rules/runner/context_match/all_rules_for_at_least_one_match.rs
@@ -1,9 +1,9 @@
-use log::{debug, error, trace};
+use log::{debug, error, info, trace};
 
 use super::*;
 use crate::core::rules::traits::RuleBase;
 use crate::core::rules::{next::NextStep, traits::CalculateValueRules, CaptureData};
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -11,7 +11,7 @@ use std::hash::Hash;
 pub fn all_rules_for_at_least_one_match<'a, R, C>(
     // this parameter is required for logs
     rule_ref: &R::RuleType,
-    // get a unique stack of one root rule, necessary to bypass the recursion constraint
+    // get a unique stack of one root cmplx_rule, necessary to bypass the recursion constraint
     stack: &mut VecDeque<(&'a R::RuleType, CaptureData<C>)>,
 ) -> NextStep
 where
@@ -42,6 +42,7 @@ where
 
                 'skip_data: for data in &frame.1.text_for_capture {
                     if let Some(simple_rules) = frame.0.get_simple_rules() {
+                        let mut selected_rules = HashSet::new();
                         for index in R::get_selected_rules(simple_rules.1, data) {
                             let rule_from_regexset = simple_rules.0.get_index(index).unwrap();
                             // ============================= LOG =============================
@@ -66,9 +67,56 @@ where
                                 err = error;
                                 continue 'skip_data;
                             }
+                            selected_rules.insert(rule_from_regexset);
                             temp_stack.push_back((rule_from_regexset, captures));
                         }
+
+                        for rule in simple_rules.0 {
+                            if !selected_rules.contains(rule) {
+                                let mut captures = R::find_captures(rule, data);
+                                if let NextStep::Error(error) =
+                                    NextStep::next_or_finish_or_error(rule, &mut captures)
+                                {
+                                    err = error;
+                                    continue 'skip_data;
+                                }
+                                temp_stack.push_back((rule, captures));
+                            }
+                        }
                     }
+                    if let Some(cmpl_rules) = frame.0.get_complex_rules() {
+                        for cmplx_rule in cmpl_rules {
+                            // ============================= LOG =============================
+                            trace!(
+                                "the rule `({}, {:#?})` from `complex_rules`",
+                                cmplx_rule.get_str(),
+                                cmplx_rule.get_requirement()
+                            );
+                            // ===============================================================
+                            let mut captures = R::find_captures(cmplx_rule, data);
+                            if let NextStep::Error(error) =
+                                NextStep::next_or_finish_or_error(cmplx_rule, &mut captures)
+                            {
+                                err = error;
+                                continue 'skip_data;
+                            }
+                            temp_stack.push_back((cmplx_rule, captures));
+                        }
+                    }
+                    info!("all rules passed successfully for the text `{:#?}` ", data);
+                    // Если дошли до конца цикла (в рамках одного элемента), значит все правила сработали
+                    rule_matched_for_any_text = true;
+                    break;
+                }
+                if rule_matched_for_any_text {
+                    // Финальный этап, мы загружаем всё в`stack` для дальнейшей обработки
+                    stack.extend(temp_stack.drain(..));
+                } else {
+                    // ================= (LOG) =================
+                    error!("all of the rules do not match any text");
+
+                    // =========================================
+                    return NextStep::Error(err);
                 }
             }
             NextStep::Finish => (),
