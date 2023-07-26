4835efba0cbc0bca2380441c3ca2825ad255afc3
Mansur Azatbek <mansur62624@gmail.com>
2023-07-26 14:20:20 +0600

    docs: updated documentation

--- src/core/rules/rule_bytes/captures.rs
+++ src/core/rules/rule_bytes/captures.rs
@@ -7,26 +7,24 @@ pub fn find_captures<'a>(rule: &RuleBytes, capture: &'a [u8]) -> CaptureData<&'a
     let mut hashmap_for_error: HashMap<String, String> = HashMap::new();
     let mut text_for_capture: HashSet<&[u8]> = HashSet::new();
     let mut counter_value: usize = 0;
-    // флаг для проверки `Counter`
+    // flag to check `Counter`
     let flag_check_counter = rule.content_unchecked().general_modifiers.counter.is_some();
-    // На первый взгляд мы видим дублирование кода, но каждый `match` работает с разными структурами
-    // создаем регулярное выражение
     let re = regex::bytes::Regex::new(&rule.content_unchecked().str_bytes.as_ref()).unwrap();
-    // получаем совпадения и повышаем `counter` по необходимости
+    // get matches and increase `counter` as necessary
     re.captures_iter(capture).for_each(|capture| {
         if let Some(value) = capture.get(0) {
             hashmap_for_error
                 .entry(DEFAULT_CAPTURE.into())
                 .or_insert_with(|| bytes_to_byte_string(value.as_bytes()));
             text_for_capture.insert(value.as_bytes());
-            // в одном `regex` может быть несколько групп, но все
-            // они нужны для получения главного совпадения, потому
-            // повышение идет только в `main capture`
+            // there can be several groups in one `regex`, but all of them
+            // they are needed to get the main match, so
+            // the increment is only in `main capture`.
             if flag_check_counter {
                 counter_value += 1;
             }
         }
-        // получаем совпадения по именам группы, для заполнения `extra` в сообщений ошибки
+        // get matches by group names to fill in the error message
         re.capture_names().for_each(|name| {
             if let Some(name) = name {
                 if let Some(value) = capture.name(name) {
--- src/core/rules/rule_str/captures.rs
+++ src/core/rules/rule_str/captures.rs
@@ -1,37 +1,34 @@
+use super::RegexRaw;
 use crate::core::rules::{traits::RuleBase, CaptureData};
 use crate::core::DEFAULT_CAPTURE;
 use crate::Rule;
 use std::collections::{HashMap, HashSet};
 
-use super::RegexRaw;
-
 pub fn find_captures<'a>(rule: &Rule, capture: &'a str) -> CaptureData<&'a str> {
     let mut hashmap_for_error = HashMap::new();
     let mut text_for_capture: HashSet<&str> = HashSet::new();
     let mut counter_value: usize = 0;
-    // флаг для проверки `Counter`
+    // flag to check `Counter`
     let flag_check_counter = rule.content_unchecked().general_modifiers.counter.is_some();
-    // На первый взгляд мы видим дублирование кода, но каждый `match` работает с разными структурами
-
+    // At first glance we see code duplication, but each `match` works with different structures
     match &rule.content_unchecked().str_with_type {
         RegexRaw::DefaultRegex(pattern) => {
-            // создаем регулярное выражение
             let re = regex::Regex::new(pattern).unwrap();
-            // получаем совпадения и повышаем `counter` по необходимости
+            // get matches and increase `counter` as necessary
             re.captures_iter(capture).for_each(|capture| {
                 if let Some(value) = capture.get(0) {
                     hashmap_for_error
                         .entry(DEFAULT_CAPTURE.into())
                         .or_insert_with(|| value.as_str().into());
                     text_for_capture.insert(value.as_str());
-                    // в одном `regex` может быть несколько групп, но все
-                    // они нужны для получения главного совпадения, потому
-                    // повышение идет только в `main capture`
+                    // there can be several groups in one `regex`, but all of them
+                    // they are needed to get the main match, so
+                    // the increment is only in `main capture`.
                     if flag_check_counter {
                         counter_value += 1;
                     }
                 }
-                // получаем совпадения по именам группы, для заполнения `extra` в сообщений ошибки
+                // get matches by group names to fill in the error message
                 re.capture_names().for_each(|name| {
                     if let Some(name) = name {
                         if let Some(value) = capture.name(name) {
@@ -44,9 +41,8 @@ pub fn find_captures<'a>(rule: &Rule, capture: &'a str) -> CaptureData<&'a str> 
             });
         }
         RegexRaw::FancyRegex(pattern) => {
-            // создаем регулярное выражение
             let re = fancy_regex::Regex::new(pattern).unwrap();
-            // получаем совпадения и повышаем `counter` по необходимости
+            // get matches and increase `counter` as necessary
             re.captures_iter(capture).for_each(|capture| {
                 if let Ok(capture) = capture {
                     if let Some(value) = capture.get(0) {
@@ -54,14 +50,14 @@ pub fn find_captures<'a>(rule: &Rule, capture: &'a str) -> CaptureData<&'a str> 
                             .entry(DEFAULT_CAPTURE.into())
                             .or_insert_with(|| value.as_str().into());
                         text_for_capture.insert(value.as_str());
-                        // в одном `regex` может быть несколько групп, но все
-                        // они нужны для получения главного совпадения, потому
-                        // повышение идет только в `main capture`
+                        // there can be several groups in one `regex`, but all of them
+                        // they are needed to get the main match, so
+                        // the increment is only in `main capture`.
                         if flag_check_counter {
                             counter_value += 1;
                         }
                     }
-                    // получаем совпадения по именам группы, для заполнения `extra` в сообщений ошибки
+                    // get matches by group names to fill in the error message
                     re.capture_names().for_each(|name| {
                         if let Some(name) = name {
                             if let Some(value) = capture.name(name) {
