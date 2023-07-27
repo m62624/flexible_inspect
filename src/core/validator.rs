d00684c570deeedd06599b019eb459cbeb6051e2
Mansur Azatbek <mansur62624@gmail.com>
2023-07-28 00:53:17 +0600

    chore: temporary template for `validator` (cartridge rule `iter`)

--- src/core/validator.rs
+++ src/core/validator.rs
@@ -2,56 +2,35 @@ use super::cartridges::CartridgeBase;
 use super::rules::traits::RuleBase;
 use crate::prelude::*;
 use std::hash::Hash;
-pub trait ValidatorBase<T, I, D>
+pub trait ValidatorBase<T, I, C, D>
 where
     T: RuleBase,
     I: IntoIterator<Item = T>,
+    C: IntoIterator<Item = dyn CartridgeBase<T, I>>,
     D: PartialEq + Eq + Hash,
 {
-    fn new(cartridges: I) -> Self;
+    fn new(cartridges: C) -> Self;
     fn validate(&self, data: D) -> Box<dyn CartridgeBase<T, I>>;
 }
 
-pub struct TemplateValidator<T, I>
+pub struct TemplateValidator<C, IC>
 where
-    T: RuleBase,
-    I: IntoIterator<Item = T>,
+    IC: IntoIterator<Item = C>,
 {
-    cartridges: I,
+    cartridges: IC,
 }
 
-impl<I> ValidatorBase<Rule, I, &str> for TemplateValidator<Rule, I>
+impl<I, C, IC> ValidatorBase<Rule, I, C, &str> for TemplateValidator<C, IC>
 where
     I: IntoIterator<Item = Rule>,
+    C: IntoIterator<Item = dyn CartridgeBase<Rule, I>>,
+    IC: IntoIterator<Item = C>,
 {
-    fn new(cartridges: I) -> Self {
-        Self { cartridges }
+    fn new(cartridges: C) -> Self {
+        todo!()
     }
 
     fn validate(&self, data: &str) -> Box<dyn CartridgeBase<Rule, I>> {
         todo!()
     }
-}
-
-impl<I> ValidatorBase<RuleBytes, I, &[u8]> for TemplateValidator<RuleBytes, I>
-where
-    I: IntoIterator<Item = RuleBytes>,
-{
-    fn new(cartridges: I) -> Self {
-        Self { cartridges }
-    }
-
-    fn validate(&self, data: &[u8]) -> Box<dyn CartridgeBase<RuleBytes, I>> {
-        todo!()
-    }
-}
-
-#[test]
-fn x() {
-    let cartridge_1 = CartridgeRule::new(
-        1,
-        "the error message from `cartridge_1`",
-        vec![Rule::new(r".+", MatchRequirement::MustBeFound)],
-    );
-    // let validator_1 = TemplateValidator::new(vec![cartridge_1]);
 }
