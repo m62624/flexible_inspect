aefdbf44b9428b6284d4bec3a3883a81dedb798f
Mansur Azatbek <mansur62624@gmail.com>
2023-07-28 00:21:45 +0600

    chore: temporary template for `validator`

--- src/core/validator.rs
+++ src/core/validator.rs
@@ -1,41 +1,61 @@
-// use super::cartridges::CartridgeBase;
-// use crate::core::rules::CaptureData;
-// use std::hash::Hash;
+use super::{
+    cartridges::{self, CartridgeBase},
+    rules::traits::RuleBase,
+};
+use crate::prelude::*;
+pub trait ValidatorBase<T, I, U, IC>
+where
+    T: RuleBase,
+    I: IntoIterator<Item = T>,
+    U: CartridgeBase<T, I>,
+    IC: IntoIterator<Item = U>,
+{
+    fn new(cartridges: IC) -> Self;
+    fn validate<S: AsRef<str>>(&self, text: S) -> Box<dyn CartridgeBase<T, I>>;
+}
 
-// pub trait PystvalValidator<C>
-// where
-//     C: PartialEq + Eq + Hash,
-// {
-//     type cartridges: CartridgeBase;
-//     type RuleCollection: CartridgeBase<RuleCollection = Self::RuleCollection>;
-//     type RuleType: CartridgeBase<RuleType = Self::RuleType>;
-//     fn template_validator<T: IntoIterator<Item = Self::cartridges>>(cartridges: T) -> Self;
-//     fn validate(
-//         &self,
-//         text: C,
-//     ) -> Box<dyn CartridgeBase<RuleCollection = Self::RuleCollection, RuleType = Self::RuleType>>;
-// }
+pub struct TemplateValidator<T, I>
+where
+    T: RuleBase,
+    I: IntoIterator<Item = T>,
+{
+    cartridges: I,
+}
 
-// pub struct TempplateValidator<T: IntoIterator<Item = T>> {
-//     cartridges: T,
-// }
+impl<I, IC> ValidatorBase<Rule, I, CartridgeRule<Rule, I>, IC> for TemplateValidator<Rule, I>
+where
+    I: IntoIterator<Item = Rule>,
+    IC: IntoIterator<Item = CartridgeRule<Rule, I>>,
+{
+    fn new(cartridges: IC) -> Self {
+        Self { cartridges }
+    }
+    fn validate<S: AsRef<str>>(&self, text: S) -> Box<dyn CartridgeBase<Rule, I>> {
+        todo!()
+    }
+}
 
-// impl<T, C> PystvalValidator<C> for TempplateValidator<T>
-// where
-//     T: CartridgeBase + IntoIterator<Item = T> + FromIterator<T>,
-//     C: PartialEq + Eq + Hash,
-// {
-//     type cartridges = T;
-//     type RuleCollection = T;
-//     type RuleType = T;
+impl<I, IC> ValidatorBase<RuleBytes, I, CartridgeRuleBytes<RuleBytes, I>, IC>
+    for TemplateValidator<RuleBytes, I>
+where
+    I: IntoIterator<Item = RuleBytes>,
+    IC: IntoIterator<Item = CartridgeRuleBytes<RuleBytes, I>>,
+{
+    fn new(cartridges: IC) -> Self {
+        Self { cartridges }
+    }
 
-//     fn template_validator<R: IntoIterator<Item = Self::cartridges>>(cartridges: R) -> Self {
-//         Self {
-//             cartridges: cartridges.into_iter().collect(),
-//         }
-//     }
+    fn validate<S: AsRef<str>>(&self, text: S) -> Box<dyn CartridgeBase<RuleBytes, I>> {
+        todo!()
+    }
+}
 
-//     fn validate(&self, text: C) {
-//         &self.cartridges;
-//     }
-// }
+#[test]
+fn x() {
+    let cartridge_1 = CartridgeRule::new(
+        1,
+        "the error message from `cartridge_1`",
+        vec![Rule::new(r".+", MatchRequirement::MustBeFound)],
+    );
+    let validator_1 = TemplateValidator::new(vec![cartridge_1]);
+}
