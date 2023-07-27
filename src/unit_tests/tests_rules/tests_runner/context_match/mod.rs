c679afaca352b1084bc60bcb042d346a9b930ac9
Mansur Azatbek <mansur62624@gmail.com>
2023-07-27 11:05:02 +0600

    test: added unit tests

--- src/unit_tests/tests_rules/tests_runner/context_match/mod.rs
+++ src/unit_tests/tests_rules/tests_runner/context_match/mod.rs
@@ -1,3 +1,4 @@
 use super::*;
+mod tests_at_least_one_rule_for_all_matches;
 mod tests_all_rules_for_all_matches;
 mod tests_all_rules_for_at_least_one_match;
---
+++ src/unit_tests/tests_rules/tests_runner/context_match/tests_at_least_one_rule_for_all_matches.rs
@@ -0,0 +1,95 @@
+use super::*;
+use crate::core::rules::{self, next::NextStep};
+
+#[test]
+fn test_runner_t_0() {
+    let text = "[ [123] [123] [1234] ] [ [123456789] [12345678] ]";
+    let rule: Rule = Rule::new(r".+", MatchRequirement::MustBeFound).extend([Rule::new(
+        r"\[[^\[\]]+\]",
+        MatchRequirement::MustBeFound,
+    )
+    .extend([
+        Rule::new(r"A", MatchRequirement::MustNotBeFound),
+        Rule::new(r"B", MatchRequirement::MustBeFound),
+        Rule::new(r"C", MatchRequirement::MustBeFound),
+    ])
+    .mode_at_least_one_rule_for_all_matches()]);
+    assert_eq!(
+        rules::runner::run::<Rule, &str>(&rule, text),
+        NextStep::Finish
+    );
+}
+
+#[test]
+fn test_runner_t_1() {
+    let text = "[ [123] [123] [1234] ] [ [123456789] [12345678] ]";
+    let rule: Rule = Rule::new(r".+", MatchRequirement::MustBeFound).extend([Rule::new(
+        r"\[[^\[\]]+\]",
+        MatchRequirement::MustBeFound,
+    )
+    .extend([
+        Rule::new(r"A", MatchRequirement::MustBeFound),
+        Rule::new(r"B", MatchRequirement::MustBeFound),
+        Rule::new(r"C", MatchRequirement::MustBeFound),
+    ])
+    .mode_at_least_one_rule_for_all_matches()]);
+    assert_eq!(
+        rules::runner::run::<Rule, &str>(&rule, text),
+        NextStep::Error(None)
+    );
+}
+
+#[test]
+fn test_runner_t_2() {
+    let text = "[ [123] [123] [1234] ] [ [123456789] [12345678] ]";
+    let rule: Rule = Rule::new(r".+", MatchRequirement::MustBeFound).extend([Rule::new(
+        r"\[[^\[\]]+\]",
+        MatchRequirement::MustBeFound,
+    )
+    .extend([
+        Rule::new(r"B", MatchRequirement::MustBeFound),
+        Rule::new(r"\d(?=\d+)", MatchRequirement::MustBeFound),
+    ])
+    .mode_at_least_one_rule_for_all_matches()]);
+    assert_eq!(
+        rules::runner::run::<Rule, &str>(&rule, text),
+        NextStep::Finish
+    );
+}
+
+#[test]
+fn test_runner_t_3() {
+    let text = "[ [123] [123] [1234] ] [ [123456789] [12345678] ]";
+    let rule: Rule = Rule::new(r".+", MatchRequirement::MustBeFound).extend([Rule::new(
+        r"\[[^\[\]]+\]",
+        MatchRequirement::MustBeFound,
+    )
+    .extend([
+        Rule::new(r"B", MatchRequirement::MustBeFound),
+        Rule::new(r"\d(?=C)", MatchRequirement::MustBeFound),
+    ])
+    .mode_at_least_one_rule_for_all_matches()]);
+    assert_eq!(
+        rules::runner::run::<Rule, &str>(&rule, text),
+        NextStep::Error(None)
+    );
+}
+
+#[test]
+fn test_runner_t_4() {
+    let text = "[ [123] [123] [1234] ] [ [123456789] [12345678] ]";
+    let rule: Rule = Rule::new(r".+", MatchRequirement::MustBeFound).extend([Rule::new(
+        r"\[[^\[\]]+\]",
+        MatchRequirement::MustBeFound,
+    )
+    .extend([
+        Rule::new(r"\d+", MatchRequirement::MustBeFound),
+        Rule::new(r"B", MatchRequirement::MustBeFound),
+        Rule::new(r"C", MatchRequirement::MustBeFound),
+    ])
+    .mode_at_least_one_rule_for_all_matches()]);
+    assert_eq!(
+        rules::runner::run::<Rule, &str>(&rule, text),
+        NextStep::Finish
+    );
+}
