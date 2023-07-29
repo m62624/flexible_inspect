75abb8cb2fa796da554169dc9e9d7f379330d8f9
Mansur Azatbek <mansur62624@gmail.com>
2023-07-29 10:19:34 +0600

    test: added unit tests

--- src/unit_tests/tests_cartridges/mod.rs
+++ src/unit_tests/tests_cartridges/mod.rs
@@ -12,7 +12,7 @@ fn test_mode_t_0() {
     );
     assert_eq!(
         cartridge
-            .root_rule()
+            .root_rule
             .content_unchecked()
             .general_modifiers
             .mod_match,
@@ -30,7 +30,7 @@ fn test_mode_t_1() {
     .mode_all_rules_for_at_least_one_match();
     assert_eq!(
         cartridge
-            .root_rule()
+            .root_rule
             .content_unchecked()
             .general_modifiers
             .mod_match,
@@ -48,7 +48,7 @@ fn test_mode_t_2() {
     .mode_at_least_one_rule_for_all_matches();
     assert_eq!(
         cartridge
-            .root_rule()
+            .root_rule
             .content_unchecked()
             .general_modifiers
             .mod_match,
@@ -66,7 +66,7 @@ fn test_mode_t_3() {
     .mode_at_least_one_rule_for_at_least_one_match();
     assert_eq!(
         cartridge
-            .root_rule()
+            .root_rule
             .content_unchecked()
             .general_modifiers
             .mod_match,
--- src/unit_tests/tests_message.rs
+++ src/unit_tests/tests_message.rs
@@ -7,7 +7,7 @@ fn test_filling_message_t_0() {
     let message_template = "Hello, {x}!".to_owned();
     let error_data = Some(HashMap::from([("x".to_owned(), "world".to_owned())]));
     assert_eq!(
-        filling_message(message_template, error_data),
+        filling_message(&message_template, error_data),
         "Hello, world!"
     );
 }
@@ -16,14 +16,14 @@ fn test_filling_message_t_0() {
 fn test_filling_message_t_1() {
     let message_template = "Hello, {x}!".to_owned();
     let error_data = Some(HashMap::from([("y".to_owned(), "world".to_owned())]));
-    assert_eq!(filling_message(message_template, error_data), "Hello, ___!");
+    assert_eq!(filling_message(&message_template, error_data), "Hello, ___!");
 }
 
 #[test]
 fn test_filling_message_t_2() {
     let message_template = "Hello, {x}!".to_owned();
     let error_data = None;
-    assert_eq!(filling_message(message_template, error_data), "Hello, ___!");
+    assert_eq!(filling_message(&message_template, error_data), "Hello, ___!");
 }
 
 #[test]
@@ -34,5 +34,5 @@ fn test_filling_message_t_3() {
         ("y".to_owned(), "2".to_owned()),
         ("z".to_owned(), "3".to_owned()),
     ]));
-    assert_eq!(filling_message(message_template, error_data), "1 2 3");
+    assert_eq!(filling_message(&message_template, error_data), "1 2 3");
 }
