2783d21239e85ce265522e5026ccf98b196eb7d3
Mansur Azatbek <mansur62624@gmail.com>
2023-07-29 10:25:39 +0600

    feat: added sync `validator`

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
+pub mod base_error;
 pub mod cartridges;
 pub mod message;
 pub mod rules;
--- src/core/validator.rs
+++ src/core/validator.rs
@@ -1,10 +1,13 @@
-use super::cartridges::{self, Cartridge, CartridgeBase};
+use super::base_error::PystvalError;
+use super::cartridges::{Cartridge, CartridgeBase};
+use super::message::filling_message;
 use super::rules::next::NextStep;
 use super::rules::traits::RuleBase;
 use super::rules::MatchRequirement;
 use crate::prelude::{Rule, RuleBytes};
 use std::fmt::Debug;
 use std::hash::Hash;
+
 pub trait ValidatorBase<R, C, IC, D>
 where
     R: RuleBase,
@@ -13,7 +16,7 @@ where
     D: PartialEq + Eq + Hash + Debug,
 {
     fn new(cartridges: IC) -> Self;
-    fn validate(&self, data: D) -> Option<Vec<Box<dyn CartridgeBase<R, D>>>>;
+    fn validate(&self, data: D) -> Result<(), Vec<PystvalError>>;
 }
 
 pub struct TemplateValidator<IC> {
@@ -29,14 +32,21 @@ where
         Self { cartridges }
     }
 
-    fn validate(&self, data: &'a str) -> Option<Vec<Box<dyn CartridgeBase<Rule, &'a str>>>> {
-        let mut result: Option<Vec<Box<dyn CartridgeBase<Rule, &'a str>>>> = Some(Vec::new());
+    fn validate(&self, data: &'a str) -> Result<(), Vec<PystvalError>> {
+        let mut error = Vec::new();
         for cartridge in self.cartridges.as_ref().into_iter() {
-            if let NextStep::Error(err) = cartridge.run(data) {
-                todo!()
+            if let NextStep::Error(extra_with_value) = cartridge.run(data) {
+                error.push(PystvalError::new(
+                    cartridge.get_id(),
+                    filling_message(cartridge.get_message(), extra_with_value),
+                ));
             }
         }
-        result
+        if error.is_empty() {
+            Ok(())
+        } else {
+            Err(error)
+        }
     }
 }
 
@@ -49,32 +59,20 @@ where
         Self { cartridges }
     }
 
-    fn validate(&self, data: &'a [u8]) -> Option<Vec<Box<dyn CartridgeBase<RuleBytes, &'a [u8]>>>> {
-        let mut result: Option<Vec<Box<dyn CartridgeBase<RuleBytes, &'a [u8]>>>> = Some(Vec::new());
+    fn validate(&self, data: &'a [u8]) -> Result<(), Vec<PystvalError>> {
+        let mut error = Vec::new();
         for cartridge in self.cartridges.as_ref().into_iter() {
-            if let NextStep::Error(err) = cartridge.run(data) {
-                todo!()
+            if let NextStep::Error(extra_with_value) = cartridge.run(data) {
+                error.push(PystvalError::new(
+                    cartridge.get_id(),
+                    filling_message(cartridge.get_message(), extra_with_value),
+                ));
             }
         }
-        result
+        if error.is_empty() {
+            Ok(())
+        } else {
+            Err(error)
+        }
     }
-}
-
-#[test]
-fn x() {
-    let cartridge_1 = Cartridge::new(
-        1,
-        "the error message from `cartridge_1`",
-        [Rule::new(r".+", MatchRequirement::MustBeFound)],
-    );
-
-    let cartridge_2 = Cartridge::new(
-        1,
-        "the error message from `cartridge_2`",
-        [Rule::new(r"\d+", MatchRequirement::MustNotBeFound)],
-    );
-
-    // let
-    let validator_1 = TemplateValidator::new([cartridge_1, cartridge_2]);
-    validator_1.validate("x");
 }
