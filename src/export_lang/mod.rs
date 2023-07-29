36bb9df298b3eb003c86c01f2d58f85a1c4a2ad0
Mansur Azatbek <mansur62624@gmail.com>
2023-07-29 23:06:49 +0600

    refactor: added path folder

--- src/export_lang/mod.rs
+++ src/export_lang/mod.rs
@@ -1,1 +1,4 @@
-
+#[cfg(feature = "python-lib")]
+mod python_version;
+#[cfg(feature = "wasm")]
+mod wasm_version;
--- src/export_lang/python_version/rules/rule_bytes/mod.rs
+++ src/export_lang/python_version/rules/rule_bytes/mod.rs
@@ -6,18 +6,18 @@ use super::{traits::PyRuleBase, *};
 #[derive(Default, Clone)]
 pub struct PyRuleBytes(RuleBytes);
 
-impl PyRuleBase for PyRuleBytes {
-    type RulTypeRust = RuleBytes;
-
-    fn to_rust(&mut self) -> RuleBytes {
-        std::mem::take(&mut self.0)
-    }
-}
-
 #[pymethods]
 impl PyRuleBytes {
     #[new]
     fn new(pattern: String, requirement: PyMatchRequirement) -> Self {
         Self(RuleBytes::new(pattern, requirement.to_rust()))
+    }
+}
+
+impl PyRuleBase for PyRuleBytes {
+    type RulTypeRust = RuleBytes;
+
+    fn to_rust(&mut self) -> RuleBytes {
+        std::mem::take(&mut self.0)
     }
 }
