679882293490362a6881d485e6a15c4fc01c8017
Mansur Azatbek <mansur62624@gmail.com>
2023-07-30 21:06:00 +0600

    feat: added `to_rust` for `CartridgeBytes`

--- src/export_lang/python_version/cartridges/cartridge_bytes/mod.rs
+++ src/export_lang/python_version/cartridges/cartridge_bytes/mod.rs
@@ -4,6 +4,8 @@ use crate::export_lang::python_version::rules::traits::PyRuleModifiers;
 use pyo3::prelude::*;
 use pyo3::pymethods;
 
+use super::traits::PyCartridgeBase;
+
 #[pyclass(name = "CartridgeBytes")]
 #[derive(Default, Clone, Debug)]
 pub struct PyCartridgeBytes(Cartridge<RuleBytes>);
@@ -17,5 +19,12 @@ impl PyCartridgeBytes {
             message,
             PyRuleBytes::_to_rust_for_extend::<PyRuleBytes>(py, root_rules, "RuleBytes")?,
         )))
+    }
+}
+
+impl PyCartridgeBase for PyCartridgeBytes {
+    type CartridgeType = Cartridge<RuleBytes>;
+    fn to_rust(&mut self) -> Self::CartridgeType {
+        std::mem::take(&mut self.0)
     }
 }
