abb28fb7767e49c6c19286214d7be8b5b305fd30
Mansur Azatbek <mansur62624@gmail.com>
2023-08-02 01:04:01 +0600

    feat: added trait for `wasm` rules

--- src/export_lang/wasm_version/rules/mod.rs
+++ src/export_lang/wasm_version/rules/mod.rs
@@ -18,3 +18,22 @@ impl From<MatchRequirement> for RustMatchRequirement {
         }
     }
 }
+
+pub trait WasmRuleModifiers {
+    type WasmRuleType;
+    type RustRuleType;
+    /// modifier to set the match counter, condition counter == match
+    fn _counter_is_equal(&mut self, count: usize) -> Self::WasmRuleType;
+    fn _counter_more_than(&mut self, count: usize) -> Self::WasmRuleType;
+
+    fn _counter_less_than(&mut self, count: usize) -> Self::WasmRuleType;
+    fn _mode_all_rules_for_at_least_one_match(&mut self) -> Self::WasmRuleType;
+    fn _mode_at_least_one_rule_for_all_matches(&mut self) -> Self::WasmRuleType;
+    fn _mode_at_least_one_rule_for_at_least_one_match(&mut self) -> Self::WasmRuleType;
+    fn _to_rust_for_extend<V, T: Into<V>>(nested_rules: Vec<T>) -> Vec<Self::RustRuleType>
+    where
+        Vec<<Self as WasmRuleModifiers>::RustRuleType>: FromIterator<V>,
+    {
+        nested_rules.into_iter().map(|rule| rule.into()).collect()
+    }
+}
