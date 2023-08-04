dc60af14094e432018bb146d47f7f1332313455b
Mansur Azatbek <mansur62624@gmail.com>
2023-08-04 23:55:34 +0600

    feat: added `wasm PystvalError`

---
+++ src/export_lang/wasm_version/base_error.rs
@@ -0,0 +1,30 @@
+use crate::core::base_error::PystvalError;
+use std::fmt;
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen(js_name = PystvalError)]
+#[derive(Debug)]
+pub struct WasmPystvalError(PystvalError);
+
+impl WasmPystvalError {
+    pub fn new(id: i32, msg: String) -> Self {
+        Self(PystvalError::new(id, msg))
+    }
+}
+
+#[wasm_bindgen(js_class = PystvalError)]
+impl WasmPystvalError {
+    pub fn get_code(&self) -> i32 {
+        self.0.id
+    }
+
+    pub fn get_message(&self) -> String {
+        self.0.msg.to_string()
+    }
+}
+
+impl fmt::Display for WasmPystvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
--- src/export_lang/wasm_version/mod.rs
+++ src/export_lang/wasm_version/mod.rs
@@ -1,6 +1,8 @@
+pub mod base_error;
 pub mod cartridges;
 pub mod rules;
 #[cfg(test)]
 pub mod unit_tests;
+pub mod validator;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::wasm_bindgen;
