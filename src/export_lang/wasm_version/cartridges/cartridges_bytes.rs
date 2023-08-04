a24e64fe7de461444340311460d03acb9881f9a4
Mansur Azatbek <mansur62624@gmail.com>
2023-08-04 20:27:53 +0600

    feat: added methods for `wasm cartridges`

--- src/export_lang/wasm_version/cartridges/cartridges_bytes.rs
+++ src/export_lang/wasm_version/cartridges/cartridges_bytes.rs
@@ -1,9 +1,7 @@
-
-
 use super::*;
 
 #[wasm_bindgen(js_name = CartridgeBytes)]
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Default)]
 pub struct WasmCartridgeBytes(pub(crate) Cartridge<RuleBytes>);
 
 #[wasm_bindgen(js_class = CartridgeBytes)]
@@ -23,5 +21,26 @@ impl WasmCartridgeBytes {
 
     pub fn finish_build(&self) -> Result<JsValue, serde_wasm_bindgen::Error> {
         serde_wasm_bindgen::to_value(&self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// `all rules` must pass the test for at least `one match`
+    pub fn mode_all_rules_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_all_rules_for_at_least_one_match();
+        std::mem::take(self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// at least `one rule` must pass the test for `all matches`
+    pub fn mode_at_least_one_rule_for_all_matches(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_all_matches();
+        std::mem::take(self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// at least `one rule` must pass the test for at least `one match`
+    pub fn mode_at_least_one_rule_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_at_least_one_match();
+        std::mem::take(self)
     }
 }
--- src/export_lang/wasm_version/cartridges/cartridges_str.rs
+++ src/export_lang/wasm_version/cartridges/cartridges_str.rs
@@ -1,8 +1,7 @@
-
 use super::*;
 
 #[wasm_bindgen(js_name = Cartridge)]
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Default)]
 pub struct WasmCartridge(pub(crate) Cartridge<Rule>);
 
 #[wasm_bindgen(js_class = Cartridge)]
@@ -22,5 +21,26 @@ impl WasmCartridge {
 
     pub fn finish_build(&self) -> Result<JsValue, serde_wasm_bindgen::Error> {
         serde_wasm_bindgen::to_value(&self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// `all rules` must pass the test for at least `one match`
+    pub fn mode_all_rules_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_all_rules_for_at_least_one_match();
+        std::mem::take(self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// at least `one rule` must pass the test for `all matches`
+    pub fn mode_at_least_one_rule_for_all_matches(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_all_matches();
+        std::mem::take(self)
+    }
+
+    /// modifier to change the rule matching mode,
+    /// at least `one rule` must pass the test for at least `one match`
+    pub fn mode_at_least_one_rule_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_at_least_one_match();
+        std::mem::take(self)
     }
 }
