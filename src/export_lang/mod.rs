e826ee7b81eda09dc9b41d3dc4d55c17946ec98a
Mansur Azatbek <mansur62624@gmail.com>
2023-08-04 17:50:41 +0600

    refactor: added macros `#[cfg(not(tarpaulin_include))]`

--- src/export_lang/mod.rs
+++ src/export_lang/mod.rs
@@ -1,8 +1,11 @@
+#[cfg(not(tarpaulin_include))]
 #[cfg(any(feature = "python-lib", feature = "wasm"))]
 pub mod core_extension;
 
+#[cfg(not(tarpaulin_include))]
 #[cfg(feature = "python-lib")]
 pub mod python_version;
 
+#[cfg(not(tarpaulin_include))]
 #[cfg(feature = "wasm")]
 pub mod wasm_version;
