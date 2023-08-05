8e4b73636264494d4274b6a0285dd3a2b297d919
Mansur Azatbek <mansur62624@gmail.com>
2023-08-06 02:19:48 +0600

    chore: temporary template TemplateValidator`

---
+++ pystval-rust/src/template_validator/mod.rs
@@ -0,0 +1,37 @@
+// =======================================================
+mod validate_bytes;
+mod validate_str;
+use crate::cartridges::traits::CartridgeBase;
+use crate::error::iterator::ValidationErrorIterator;
+// =======================================================
+#[cfg(feature = "serde")]
+use super::{Deserialize, Serialize};
+use crate::rules::next::NextStep;
+use async_trait::async_trait;
+use std::fmt::Debug;
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+/// Use trait for `overloading` methods of `&str` and `&[u8]` types
+#[async_trait]
+pub trait ValidatorBase<C, IC, D>
+where
+    C: CartridgeBase<D> + Sync + Debug,
+    IC: IntoIterator<Item = C> + Sync + Send,
+    D: PartialEq + Eq + Hash + Debug,
+{
+    fn new(cartridges: IC) -> Self;
+    fn validate(&self, data: D) -> Result<(), ValidationErrorIterator>;
+    async fn async_validate(&self, data: D) -> Result<(), ValidationErrorIterator>;
+}
+
+/// The structure for creating unique validators, load different `cartridges` to validate data.
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone)]
+pub struct TemplateValidator<IC, D>
+where
+    D: PartialEq + Eq + Hash + Debug,
+{
+    pub(crate) cartridges: IC,
+    _phantom: PhantomData<D>,
+}
---
+++ pystval-rust/src/template_validator/validate_str.rs
@@ -0,0 +1,57 @@
+use super::*;
+use crate::error::{traits::ValidationError, BaseValidationError};
+use crate::message::filling_message;
+use futures::StreamExt;
+
+#[async_trait]
+impl<'a, C, IC> ValidatorBase<C, IC, &'a str> for TemplateValidator<IC, &'a str>
+where
+    C: CartridgeBase<&'a str> + Debug + Sync,
+    IC: IntoIterator<Item = C> + AsRef<[C]> + Sync + Send,
+{
+    fn new(cartridges: IC) -> Self {
+        Self {
+            cartridges,
+            _phantom: PhantomData,
+        }
+    }
+
+    fn validate(&self, data: &'a str) -> Result<(), ValidationErrorIterator> {
+        let mut error = Vec::new();
+        for cartridge in self.cartridges.as_ref().iter() {
+            if let NextStep::Error(extra_with_value) = cartridge.run(data) {
+                error.push(Box::new(BaseValidationError::new(
+                    cartridge.get_id(),
+                    filling_message(cartridge.get_message(), extra_with_value),
+                )) as Box<dyn ValidationError + Send>);
+            }
+        }
+        if error.is_empty() {
+            Ok(())
+        } else {
+            Err(ValidationErrorIterator::new(error))
+        }
+    }
+
+    async fn async_validate(&self, data: &'a str) -> Result<(), ValidationErrorIterator> {
+        let error = futures::stream::iter(self.cartridges.as_ref().into_iter())
+            .filter_map(|cartridge| async move {
+                if let NextStep::Error(extra_with_value) = cartridge.run(&data) {
+                    Some(Box::new(BaseValidationError::new(
+                        cartridge.get_id(),
+                        filling_message(cartridge.get_message(), extra_with_value),
+                    )) as Box<dyn ValidationError + Send>)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
+            .await;
+
+        if error.is_empty() {
+            Ok(())
+        } else {
+            Err(ValidationErrorIterator::new(error))
+        }
+    }
+}
