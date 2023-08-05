4adcd7c5d88ac0ecb08f32b7b964cd7e1b1200db
Mansur Azatbek <mansur62624@gmail.com>
2023-08-06 02:19:15 +0600

    refactor: added `send` marker

--- pystval-rust/src/error/iterator.rs
+++ pystval-rust/src/error/iterator.rs
@@ -2,35 +2,35 @@ use super::traits::ValidationError;
 use futures::Stream;
 
 pub struct ValidationErrorIterator {
-    collection: Vec<Box<dyn ValidationError>>,
+    collection: Vec<Box<dyn ValidationError + Send>>,
 }
 
 impl ValidationErrorIterator {
-    pub fn new(collection: Vec<Box<dyn ValidationError>>) -> Self {
+    pub fn new(collection: Vec<Box<dyn ValidationError + Send>>) -> Self {
         Self { collection }
     }
 
-    pub async fn async_into_iter(self) -> impl Stream<Item = Box<dyn ValidationError>> {
+    pub async fn async_into_iter(self) -> impl Stream<Item = Box<dyn ValidationError + Send>> {
         futures::stream::iter(self.collection.into_iter())
     }
 }
 
 impl Iterator for &mut ValidationErrorIterator {
-    type Item = Box<dyn ValidationError>;
+    type Item = Box<dyn ValidationError + Send>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.collection.pop()
     }
 }
 
-impl AsRef<Vec<Box<dyn ValidationError>>> for ValidationErrorIterator {
-    fn as_ref(&self) -> &Vec<Box<dyn ValidationError>> {
+impl AsRef<Vec<Box<dyn ValidationError+ Send>>> for ValidationErrorIterator {
+    fn as_ref(&self) -> &Vec<Box<dyn ValidationError + Send>> {
         &self.collection
     }
 }
 
 impl IntoIterator for ValidationErrorIterator {
-    type Item = Box<dyn ValidationError>;
+    type Item = Box<dyn ValidationError + Send>;
     type IntoIter = std::vec::IntoIter<Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
