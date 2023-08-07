991832764ad43645ee3f9f282b217feffeedb65f
Mansur Azatbek <mansur62624@gmail.com>
2023-08-07 12:32:40 +0600

    feat: added `template_validator`

--- pystval-python/src/template_validator/validate_bytes.rs
+++ pystval-python/src/template_validator/validate_bytes.rs
@@ -1,21 +1,20 @@
-use crate::error::PyValidationErrorIterator;
-
 use super::*;
+use crate::error::PyValidationErrorIterator;
 use std::sync::Arc;
 
 #[pyclass(name = "TemplateValidatorBytes")]
-pub struct PyTemplateValidatorBytes(TemplateValidator<Vec<Cartridge<RuleBytes>>, Arc<[u8]>>);
+pub struct PyTemplateValidatorBytes(Arc<TemplateValidator<Vec<Cartridge<RuleBytes>>, Arc<[u8]>>>);
 
 #[pymethods]
 impl PyTemplateValidatorBytes {
     #[new]
     pub fn new(cartridges: Vec<PyCartridgeBytes>) -> Self {
-        Self(TemplateValidator::new(
+        Self(Arc::new(TemplateValidator::new(
             cartridges
                 .into_iter()
                 .map(|cartridge| cartridge.into())
                 .collect(),
-        ))
+        )))
     }
 
     pub fn validate(&self, data: &[u8]) -> PyValidationErrorIterator {
@@ -25,9 +24,11 @@ impl PyTemplateValidatorBytes {
         PyValidationErrorIterator::new(vec![])
     }
 
-    pub fn async_validate<'py>(&self, py: Python, data: &[u8]) -> PyResult<&'py PyAny> {
+    pub fn async_validate<'py>(&self, py: Python<'py>, data: &[u8]) -> PyResult<&'py PyAny> {
+        let safety_self = Arc::clone(&self.0);
+        let safety_data = Arc::from(data);
         pyo3_asyncio::async_std::future_into_py(py, async move {
-            if let Err(value) = self.0.async_validate(data.into()).await {
+            if let Err(value) = safety_self.async_validate(safety_data).await {
                 return Ok(PyValidationErrorIterator::new(value.into_iter().collect()));
             }
             Ok(PyValidationErrorIterator::new(vec![]))
---
+++ pystval-python/src/template_validator/validate_str.rs
@@ -0,0 +1,37 @@
+use super::*;
+use crate::error::PyValidationErrorIterator;
+use std::sync::Arc;
+
+#[pyclass(name = "TemplateValidator")]
+pub struct PyTemplateValidator(Arc<TemplateValidator<Vec<Cartridge<Rule>>, Arc<str>>>);
+
+#[pymethods]
+impl PyTemplateValidator {
+    #[new]
+    pub fn new(cartridges: Vec<PyCartridge>) -> Self {
+        Self(Arc::new(TemplateValidator::new(
+            cartridges
+                .into_iter()
+                .map(|cartridge| cartridge.into())
+                .collect(),
+        )))
+    }
+
+    pub fn validate(&self, data: String) -> PyValidationErrorIterator {
+        if let Err(value) = self.0.validate(data.into()) {
+            return PyValidationErrorIterator::new(value.into_iter().collect());
+        }
+        PyValidationErrorIterator::new(vec![])
+    }
+
+    pub fn async_validate<'py>(&self, py: Python<'py>, data: String) -> PyResult<&'py PyAny> {
+        let safety_self = Arc::clone(&self.0);
+        let safety_data = Arc::from(data);
+        pyo3_asyncio::async_std::future_into_py(py, async move {
+            if let Err(value) = safety_self.async_validate(safety_data).await {
+                return Ok(PyValidationErrorIterator::new(value.into_iter().collect()));
+            }
+            Ok(PyValidationErrorIterator::new(vec![]))
+        })
+    }
+}
