0af5575bcf4ce3135e6a5bc3e1e648ff3aacd0cc
Mansur Azatbek <mansur62624@gmail.com>
2023-08-07 11:40:26 +0600

    refactor: added modifiers for `wasm_cartridges`

---
+++ pystval-wasm/src/cartridges/cartridge_bytes/modifiers.rs
@@ -0,0 +1,19 @@
+use super::*;
+
+#[wasm_bindgen(js_class=CartridgeBytes)]
+impl WasmCartridgeBytes {
+    pub fn mode_all_rules_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_all_rules_for_at_least_one_match();
+        std::mem::take(self)
+    }
+
+    pub fn mode_at_least_one_rule_for_all_matches(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_all_matches();
+        std::mem::take(self)
+    }
+
+    pub fn mode_at_least_one_rule_for_at_least_one_match(&mut self) -> Self {
+        self.0 = self.0.mode_at_least_one_rule_for_at_least_one_match();
+        std::mem::take(self)
+    }
+}
