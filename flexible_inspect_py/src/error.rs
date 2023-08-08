70a406c92c5577ea4b97afb909e65ca909363e3b
Mansur Azatbek <mansur62624@gmail.com>
2023-08-08 12:08:58 +0600

    refactor: changed methods of the `iterator_errors`

--- flexible_inspect_py/src/error.rs
+++ flexible_inspect_py/src/error.rs
@@ -23,44 +23,56 @@ pub struct PyValidationErrorIterator(Vec<Box<dyn ValidationError + Send>>);
 
 #[pymethods]
 impl PyValidationErrorIterator {
-    pub fn next(&mut self) -> Option<PyBaseValidationError> {
-        self.0.pop().map(|error| PyBaseValidationError {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    pub fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<PyBaseValidationError> {
+        slf.0.pop().map(|error| PyBaseValidationError {
             code: error.get_code(),
             message: error.get_message().to_owned(),
         })
     }
 
-    pub fn for_each(&self, py: Python, callback: PyObject) -> PyResult<Vec<PyObject>> {
-        self.0.iter().try_fold(Vec::new(), |mut acc, item| {
-            let result = callback.call1(py, (item.get_code(), item.get_message()))?;
-            acc.push(result);
-            Ok(acc)
-        })
-    }
+    // pub fn for_each(&self, py: Python, callback: PyObject) -> PyResult<Vec<PyObject>> {
+    //     self.0.iter().try_fold(Vec::new(), |mut acc, item| {
+    //         let result = callback.call1(py, (item.get_code(), item.get_message()))?;
+    //         acc.push(result);
+    //         Ok(acc)
+    //     })
+    // }
 
-    pub fn if_error(&self, py: Python, callback: PyObject) -> PyResult<Vec<PyObject>> {
-        if self.0.is_empty() {
-            Ok(Vec::new())
-        } else {
-            self.for_each(py, callback)
-        }
-    }
+    // pub fn for_each_with_reserved_params(&self, py: Python, callback: PyObject) -> PyResult<Vec<PyObject>> {
+    //     self.0.iter().try_fold(Vec::new(), |mut acc, item| {
+    //         let result = callback.call1(py, (item.get_code(), item.get_message()))?;
+    //         acc.push(result);
+    //         Ok(acc)
+    //     })
+    // }
 
-    pub fn if_ok(&self, py: Python, callback: PyObject) -> PyResult<PyObject> {
-        if self.0.is_empty() {
-            Ok(callback.call0(py)?)
-        } else {
-            Ok(py.None())
-        }
-    }
+    // pub fn if_error_with_reserved_params(&self, py: Python, callback: PyObject) -> PyResult<Vec<PyObject>> {
+    //     if self.0.is_empty() {
+    //         Ok(Vec::new())
+    //     } else {
+    //         self.for_each_with_reserved_params(py, callback)
+    //     }
+    // }
 
-    pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
-    }
+    // pub fn if_ok(&self, py: Python, callback: PyObject) -> PyResult<PyObject> {
+    //     if self.0.is_empty() {
+    //         Ok(callback.call0(py)?)
+    //     } else {
+    //         Ok(py.None())
+    //     }
+    // }
 
-    pub fn len(&self) -> usize {
-        self.0.len()
-    }
+    // pub fn is_empty(&self) -> bool {
+    //     self.0.is_empty()
+    // }
+
+    // pub fn len(&self) -> usize {
+    //     self.0.len()
+    // }
 }
 
 impl PyValidationErrorIterator {
