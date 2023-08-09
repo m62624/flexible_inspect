87602a1efc5d7d246f65da32ee12fe3aefdef487
Mansur Azatbek <mansur62624@gmail.com>
2023-08-09 20:16:40 +0600

    test: added unit tests

---
+++ flexible_inspect_rs/src/unit_tests/tests_next.rs
@@ -0,0 +1,136 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    prelude::*,
+    rules::{next::NextStep, CaptureData},
+};
+
+/// true, true - MustBeFound
+#[test]
+fn test_next_runner_t_0() {
+    let rule = Rule::new(r".+", MatchRequirement::MustBeFound)
+        .extend([Rule::new(r"x", MatchRequirement::MustBeFound)]);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::from([String::from("x")]),
+        hashmap_for_error: Default::default(),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Go
+    );
+}
+
+/// true, false - MustBeFound
+#[test]
+fn test_next_runner_t_1() {
+    let rule = Rule::new(r".+", MatchRequirement::MustBeFound);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::from([String::from("x")]),
+        hashmap_for_error: Default::default(),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Finish
+    );
+}
+
+/// false, true - MustBeFound
+#[test]
+fn test_next_runner_t_2() {
+    let rule = Rule::new(r".+", MatchRequirement::MustBeFound)
+        .extend([Rule::new(r"x", MatchRequirement::MustBeFound)]);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::new(),
+        hashmap_for_error: Default::default(),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Error(None)
+    );
+}
+
+/// false, false - MustBeFound
+#[test]
+fn test_next_runner_t_3() {
+    let rule = Rule::new(r".+", MatchRequirement::MustBeFound);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::new(),
+        hashmap_for_error: Default::default(),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Error(None)
+    );
+}
+
+/// true, true - MustNotBeFound
+#[test]
+fn test_next_runner_t_4() {
+    let rule = Rule::new(r".+", MatchRequirement::MustNotBeFound)
+        .extend([Rule::new(r"x", MatchRequirement::MustNotBeFound)]);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::from([String::from("x")]),
+        hashmap_for_error: Default::default(),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Go
+    );
+}
+
+/// true, false - MustNotBeFound
+#[test]
+fn test_next_runner_t_5() {
+    let rule = Rule::new(r".+", MatchRequirement::MustNotBeFound);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::from([String::from("x")]),
+        hashmap_for_error: HashMap::from([(String::from("x"), String::from("Y"))]),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Error(Some(HashMap::from([(
+            String::from("x"),
+            String::from("Y")
+        )])))
+    );
+}
+
+/// false, true - MustNotBeFound
+#[test]
+fn test_next_runner_t_6() {
+    let rule = Rule::new(r".+", MatchRequirement::MustNotBeFound).extend([Rule::new(
+        r"x",
+        MatchRequirement::MustNotBeFound,
+    )]);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::new(),
+        hashmap_for_error: HashMap::from([(String::from("x"), String::from("Y"))]),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Finish
+    );
+}
+
+
+/// false, false - MustNotBeFound
+#[test]
+fn test_next_runner_t_7() {
+    let rule = Rule::new(r".+", MatchRequirement::MustNotBeFound);
+    let mut capture: CaptureData<String> = CaptureData {
+        text_for_capture: HashSet::new(),
+        hashmap_for_error: HashMap::from([(String::from("x"), String::from("Y"))]),
+        counter_value: Default::default(),
+    };
+    assert_eq!(
+        NextStep::next_or_finish_or_error(&rule, &mut capture),
+        NextStep::Finish
+    );
+}
--- flexible_inspect_rs/src/unit_tests/tests_validate/mod.rs
+++ flexible_inspect_rs/src/unit_tests/tests_validate/mod.rs
@@ -1,4 +1,5 @@
 mod validate_bytes;
 mod validate_str;
 use crate::prelude::*;
+#[cfg(feature = "export_to_other_languages")]
 use std::sync::Arc;
