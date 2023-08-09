364597f9999bbb5d59b633fbbf954320811df6cb
Mansur Azatbek <mansur62624@gmail.com>
2023-08-09 10:29:20 +0600

    test: changed test

--- flexible_inspect_py/tests/api.py
+++ flexible_inspect_py/tests/api.py
@@ -1,24 +1,13 @@
 from flexible_inspect_py import Cartridge, CartridgeBytes, MatchRequirement, TemplateValidator, TemplateValidatorBytes, Rule, RuleBytes
 
+error_404 = Cartridge(404, "Not Found", [Rule(
+    r"/.+/.+/file.txt", MatchRequirement.MustBeFound)])
 
-def my_function(x, y):
-    print("Error code: ", "Message: ", x, y)
-
-
-error_404 = Cartridge(404,
-                      "Not Found",
-                      [Rule(r"/.+/.+/file.txt", MatchRequirement.MustBeFound)]
-                      )
-
-error_panic_system = Cartridge(500,
-                               "Internal Error {G}",
-                               [Rule(r"(?P<G>^(?!.*secret_info).*$)",
-                                     MatchRequirement.MustNotBeFound)]
-                               )
+error_panic_system = Cartridge(500, "Internal Error {G}", [Rule(
+    r"(?P<G>^(?!.*secret_info).*$)", MatchRequirement.MustNotBeFound)])
 
 valdiator_system = TemplateValidator([error_404, error_panic_system])
 iterator_rust = valdiator_system.validate("10------10")
 
 for error in iterator_rust:
     print(error.get_code(), error.get_message())
-# print(iterator_rust.__next__().get_code())
