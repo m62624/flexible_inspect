3c22a4c4e11a46216bf53e7c4e25583425a3bda9
Mansur Azatbek <mansur62624@gmail.com>
2023-08-09 22:25:56 +0600

    test: added unit tests

--- flexible_inspect_py/src/unit_tests/tests_cartridges/test_cartridges_str.rs
+++ flexible_inspect_py/src/unit_tests/tests_cartridges/test_cartridges_str.rs
@@ -18,3 +18,66 @@ fn test_new_t_0() {
         ),
     )
 }
+
+#[test]
+fn test_mode_match_t_0() {
+    let cartridge: Cartridge<Rule> = PyCartridge::new(
+        0,
+        "message_0".into(),
+        vec![PyRule::new(r"\w+".into(), PyMatchRequeriment::MustBeFound)],
+    )
+    .mode_all_rules_for_at_least_one_match()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![Rule::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_all_rules_for_at_least_one_match(),
+    )
+}
+
+#[test]
+fn test_mode_match_t_1() {
+    let cartridge: Cartridge<Rule> = PyCartridge::new(
+        0,
+        "message_0".into(),
+        vec![PyRule::new(r"\w+".into(), PyMatchRequeriment::MustBeFound)],
+    )
+    .mode_at_least_one_rule_for_all_matches()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![Rule::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_at_least_one_rule_for_all_matches(),
+    )
+}
+
+#[test]
+fn test_mode_match_t_2() {
+    let cartridge: Cartridge<Rule> = PyCartridge::new(
+        0,
+        "message_0".into(),
+        vec![PyRule::new(r"\w+".into(), PyMatchRequeriment::MustBeFound)],
+    )
+    .mode_at_least_one_rule_for_at_least_one_match()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![Rule::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_at_least_one_rule_for_at_least_one_match(),
+    )
+}
--- flexible_inspect_py/src/unit_tests/tests_cartridges/tests_cartridges_bytes.rs
+++ flexible_inspect_py/src/unit_tests/tests_cartridges/tests_cartridges_bytes.rs
@@ -4,7 +4,10 @@ fn test_new_t_0() {
     let cartridge: Cartridge<RuleBytes> = PyCartridgeBytes::new(
         0,
         "message_0".into(),
-        vec![PyRuleBytes::new(r"\w+".into(), PyMatchRequeriment::MustBeFound)],
+        vec![PyRuleBytes::new(
+            r"\w+".into(),
+            PyMatchRequeriment::MustBeFound,
+        )],
     )
     .into();
 
@@ -15,5 +18,77 @@ fn test_new_t_0() {
             "message_0",
             vec![RuleBytes::new(r"\w+", MatchRequirement::MustBeFound)],
         ),
+    )
+}
+
+#[test]
+fn test_mode_match_t_0() {
+    let cartridge: Cartridge<RuleBytes> = PyCartridgeBytes::new(
+        0,
+        "message_0".into(),
+        vec![PyRuleBytes::new(
+            r"\w+".into(),
+            PyMatchRequeriment::MustBeFound,
+        )],
+    )
+    .mode_all_rules_for_at_least_one_match()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![RuleBytes::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_all_rules_for_at_least_one_match(),
+    )
+}
+
+#[test]
+fn test_mode_match_t_1() {
+    let cartridge: Cartridge<RuleBytes> = PyCartridgeBytes::new(
+        0,
+        "message_0".into(),
+        vec![PyRuleBytes::new(
+            r"\w+".into(),
+            PyMatchRequeriment::MustBeFound,
+        )],
+    )
+    .mode_at_least_one_rule_for_all_matches()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![RuleBytes::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_at_least_one_rule_for_all_matches(),
+    )
+}
+
+#[test]
+fn test_mode_match_t_2() {
+    let cartridge: Cartridge<RuleBytes> = PyCartridgeBytes::new(
+        0,
+        "message_0".into(),
+        vec![PyRuleBytes::new(
+            r"\w+".into(),
+            PyMatchRequeriment::MustBeFound,
+        )],
+    )
+    .mode_at_least_one_rule_for_at_least_one_match()
+    .into();
+
+    assert_eq!(
+        cartridge,
+        Cartridge::new(
+            0,
+            "message_0",
+            vec![RuleBytes::new(r"\w+", MatchRequirement::MustBeFound)],
+        )
+        .mode_at_least_one_rule_for_at_least_one_match(),
     )
 }
