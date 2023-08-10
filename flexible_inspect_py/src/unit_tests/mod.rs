0af86c5e4d24050a547656312e7c14a61e1f4f6a
Mansur Azatbek <mansur62624@gmail.com>
2023-08-10 12:56:14 +0600

    test: added unit tests

--- flexible_inspect_py/src/unit_tests/mod.rs
+++ flexible_inspect_py/src/unit_tests/mod.rs
@@ -1,3 +1,4 @@
 use super::*;
 mod tests_rules;
 mod tests_cartridges;
+mod tests_validate;
---
+++ flexible_inspect_py/src/unit_tests/tests_validate.rs
@@ -0,0 +1,51 @@
+use crate::{
+    PyCartridge, PyCartridgeBytes, PyMatchRequeriment, PyRule, PyRuleBytes, PyTemplateValidator,
+    PyTemplateValidatorBytes,
+};
+
+#[test]
+fn test_validate_t_0() {
+    let cartrdige_1 = PyCartridge::new(
+        1,
+        "message_1".into(),
+        vec![PyRule::new(
+            r"ABC".into(),
+            PyMatchRequeriment::MustNotBeFound,
+        )],
+    );
+
+    let cartridge_2 = PyCartridge::new(
+        2,
+        "message_2".into(),
+        vec![PyRule::new(r"\d+".into(), PyMatchRequeriment::MustBeFound)],
+    );
+    let validator = PyTemplateValidator::new(vec![cartrdige_1, cartridge_2]);
+    assert!(validator.validate("123".to_string()).0.is_empty());
+    assert!(!validator.validate("ABC 123".to_string()).0.is_empty());
+    assert_eq!(validator.validate("ABC".to_string()).0.len(), 2);
+}
+
+#[test]
+fn test_validate_t_1() {
+    let cartrdige_1 = PyCartridgeBytes::new(
+        1,
+        "message_1".into(),
+        vec![PyRuleBytes::new(
+            r"ABC".into(),
+            PyMatchRequeriment::MustNotBeFound,
+        )],
+    );
+
+    let cartridge_2 = PyCartridgeBytes::new(
+        2,
+        "message_2".into(),
+        vec![PyRuleBytes::new(
+            r"\d+".into(),
+            PyMatchRequeriment::MustBeFound,
+        )],
+    );
+    let validator = PyTemplateValidatorBytes::new(vec![cartrdige_1, cartridge_2]);
+    assert!(validator.validate("123".as_bytes()).0.is_empty());
+    assert!(!validator.validate("ABC 123".as_bytes()).0.is_empty());
+    assert_eq!(validator.validate("ABC".as_bytes()).0.len(), 2);
+}
