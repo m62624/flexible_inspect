a71e94a76edcb6b39d28592ae9f88e9e8af54e7e
Mansur Azatbek <mansur62624@gmail.com>
2023-08-12 21:52:18 +0600

    test: added unit tests

--- flexible_inspect_js/src/unit_tests/mod.rs
+++ flexible_inspect_js/src/unit_tests/mod.rs
@@ -2,3 +2,4 @@ use super::*;
 use wasm_bindgen_test::wasm_bindgen_test;
 mod tests_cartrdiges;
 mod tests_rules;
+mod tests_validate;
---
+++ flexible_inspect_js/src/unit_tests/tests_validate.rs
@@ -0,0 +1,63 @@
+use super::*;
+
+#[wasm_bindgen_test]
+fn test_validate_t_0() {
+    let cartrdige_1 = WasmCartridge::new(
+        1,
+        "message_1".into(),
+        serde_wasm_bindgen::to_value(&vec![WasmRule::new(
+            js_sys::RegExp::new(r"ABC", ""),
+            WasmMatchRequirement::MustNotBeFound,
+        )])
+        .unwrap(),
+    )
+    .unwrap();
+    let cartridge_2 = WasmCartridge::new(
+        2,
+        "message_2".into(),
+        serde_wasm_bindgen::to_value(&vec![WasmRule::new(
+            js_sys::RegExp::new(r"\d+", ""),
+            WasmMatchRequirement::MustBeFound,
+        )])
+        .unwrap(),
+    )
+    .unwrap();
+    let validator = WasmTemplateValidator::new(
+        serde_wasm_bindgen::to_value(&vec![cartrdige_1, cartridge_2]).unwrap(),
+    )
+    .unwrap();
+    assert!(validator.validate("123".to_string()).is_none());
+    assert!(!validator.validate("ABC 123".to_string()).is_none());
+    assert_eq!(validator.validate("ABC".to_string()).unwrap().0.len(), 2);
+}
+
+#[wasm_bindgen_test]
+fn test_validate_t_1() {
+    let cartrdige_1 = WasmCartridgeBytes::new(
+        1,
+        "message_1".into(),
+        serde_wasm_bindgen::to_value(&vec![WasmRuleBytes::new(
+            js_sys::RegExp::new(r"ABC", ""),
+            WasmMatchRequirement::MustNotBeFound,
+        )])
+        .unwrap(),
+    )
+    .unwrap();
+    let cartridge_2 = WasmCartridgeBytes::new(
+        2,
+        "message_2".into(),
+        serde_wasm_bindgen::to_value(&vec![WasmRuleBytes::new(
+            js_sys::RegExp::new(r"\d+", ""),
+            WasmMatchRequirement::MustBeFound,
+        )])
+        .unwrap(),
+    )
+    .unwrap();
+    let validator = WasmTemplateValidatorBytes::new(
+        serde_wasm_bindgen::to_value(&vec![cartrdige_1, cartridge_2]).unwrap(),
+    )
+    .unwrap();
+    assert!(validator.validate("123".as_bytes()).is_none());
+    assert!(!validator.validate("ABC 123".as_bytes()).is_none());
+    assert_eq!(validator.validate("ABC".as_bytes()).unwrap().0.len(), 2);
+}
