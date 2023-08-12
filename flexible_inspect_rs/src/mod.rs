f40145afed3d64038054d4fd248b4199ead5f180
Mansur Azatbek <mansur62624@gmail.com>
2023-08-12 16:54:00 +0600

    chore: deleted unnecessary file

--- flexible_inspect_rs/src/mod.rs
+++
@@ -1,8 +0,0 @@
-// =======================================================
-pub mod base_error;
-pub mod cartridges;
-pub mod message;
-pub mod rules;
-pub mod validator;
-// =======================================================
-
