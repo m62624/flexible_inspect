6cdf7e2f404aeecf76a810e65a09908aaf6afc18
Mansur Azatbek <mansur62624@gmail.com>
2023-08-13 23:58:26 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/rules/mod.rs
+++ flexible_inspect_rs/src/rules/mod.rs
@@ -38,6 +38,22 @@ pub struct GeneralModifiers {
 }
 
 /// The structure that defines what action is required when finding regular expression matches.
+/// ## MatchRequirement
+/// - `MustBeFound` - which means we must necessarily get a match from this regular expression
+/// - `MustNotBeFound` - which means, based on this regular expression, we must not get a match
+///
+/// Behavior of `Rule` | `RuleBytes` under different conditions
+/// 
+/// | **MatchRequirement** | **Match found** | *does this rule have any subrules ?* | Result                                   |
+/// | ---------------- | ----------- | ---------------------------------- | ---------------------------------------- |
+/// | MustBeFound      | Yes         | Yes                                | Continue processing subrules             |
+/// | MustBeFound      | Yes         | No                                 | Finish processing                        |
+/// | MustBeFound      | No          | Yes                                | Error (match must have been found)       |
+/// | MustBeFound      | No          | No                                 | Error (match must have been found)       |
+/// | MustNotBeFound   | Yes         | Yes                                | Continue processing subrules             |
+/// | MustNotBeFound   | Yes         | No                                 | Error (match should not have been found) |
+/// | MustNotBeFound   | No          | Yes                                | Finish processing                        |
+/// | MustNotBeFound   | No          | No                                 | Finish processing                        |
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum MatchRequirement {
