8767d0805b018032881f308bff39806c85b83c4c
Mansur Azatbek <mansur62624@gmail.com>
2023-08-14 00:34:37 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/rules/rule_str/init.rs
+++ flexible_inspect_rs/src/rules/rule_str/init.rs
@@ -7,15 +7,32 @@ impl Rule {
     /// Constructor for creating `Rule`
     ///
     /// **Notes**:\
-    /// Please stick to *raw string literals* when creating regular expressions, without it your regular expression may behave differently
+    /// * Please stick to *raw string literals* when creating regular expressions, without it your regular expression may behave differently
     /// # Example:
-    /// > * **`r`**`"d{3}."` - is the correct conversion to a regular expression
+    /// > * `r"d{3}."` - is the correct conversion to a regular expression
     /// > * `"d{3}."` - possible incorrect behavior
     /// ```rust
     /// # use flexible_inspect_rs::prelude::*;
     /// let rule1 = Rule::new(r"\d+", MatchRequirement::MustBeFound);
     /// let rule2 = Rule::new(r"\s+", MatchRequirement::MustNotBeFound);
     /// ```
+    /// * **By default, all rules must pass every match check**
+    /// In this mode, to which all additional rules apply (default mode for everyone).
+    /// We check that for each match (text) all the rules will work.
+    /// ## Operation scheme of the mode
+    /// ```bash
+    /// #=======================================
+    /// text = "txt [123] txt [456] txt [789]"
+    /// #=======================================
+    /// CustomError
+    /// |
+    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
+    ///      |   [123], [456], [789]
+    ///      |___ Subrule ".+" (MustBeFound) ---> [123] -> [456] -> [789] -- TRUE
+    ///      |                                      |       |        |
+    ///      |___ Subrule "\[\d+\]" (MustBeFound) __|_______|________|
+    /// ```
+    ///
     pub fn new<T: Into<String>>(pattern: T, requirement: MatchRequirement) -> Self {
         #[cfg(feature = "log_rust")]
         {
