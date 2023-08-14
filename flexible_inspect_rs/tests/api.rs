4116cdec3d561cc31281cb24cc93d005c6cfb88f
Mansur Azatbek <mansur62624@gmail.com>
2023-08-14 15:44:24 +0600

    test: changed tests

--- flexible_inspect_rs/tests/api.rs
+++ flexible_inspect_rs/tests/api.rs
@@ -6,64 +6,51 @@ mod api_rules {
     #[async_std::test]
     async fn check_bash() {
         let bash_script = r###"
-#!/bin/bash
+#!/bin/bash 
 
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <number> <word> <username>"
     exit 1
-fi
+fi 
 
 if [ "$1" -eq "$1" ] 2>/dev/null; then
     for ((i=0; i<$1; i++)); do
         echo "Short message for user $3"
     done
-fi
+fi 
 
 if [ -n "$2" ]; then
     echo "Word: $2"
-fi
+fi 
 
 if [ -n "$3" ]; then
     echo "Warning: Using sudo_su as $3 can be dangerous"
     sudo rm 1234_important_file.txt
-fi
+fi 
 
-if [[ -n "$4" && "$4" =~ ^[0-9]{8,}$ ]]; then
-    echo "Password set: $4"
+if [[ -n "$4" && "$4" =~ ^[0-9]{8,}$ ]]; then 
+    echo "Password set: $4" 
 fi        
 "###;
 
-        let forbidden_sudo = Cartridge::new(
+        let error_bash_file = Cartridge::new(
             0,
             "Scripts with increased access are forbidden : {SUDO_OR_SU}",
             [
                 Rule::new(r"(?P<SUDO_OR_SU>sudo .+)", MatchRequirement::MustNotBeFound),
                 Rule::new(r"(?P<SUDO_OR_SU>su .+)", MatchRequirement::MustNotBeFound),
+                Rule::new(r".+", MatchRequirement::MustBeFound)
+                    .extend([
+                        Rule::new(".+", MatchRequirement::MustBeFound)
+                            .extend([Rule::new(r"aboba", MatchRequirement::MustBeFound)]),
+                        Rule::new(r"\s", MatchRequirement::MustBeFound),
+                        Rule::new(r"(?P<USERNAME>\for .+\n)", MatchRequirement::MustBeFound),
+                    ])
+                    .any_r_for_all_m(),
             ],
-        );
-        let incorrect_header = Cartridge::new(
-            1,
-            "Inccorect type file",
-            [Rule::new(
-                r"^\n?#!/bin/bash\s+?\n?",
-                MatchRequirement::MustBeFound,
-            )],
-        );
-
-        let incorrect_body = Cartridge::new(
-            2,
-            "Inccorect fragment code",
-            [
-                Rule::new(r"(?s).+", MatchRequirement::MustBeFound).extend([Rule::new(
-                    r"if.+then",
-                    MatchRequirement::MustBeFound,
-                )
-                .counter_is_equal(5)]),
-            ],
-        );
-
-        let validator_bash =
-            TemplateValidator::new(vec![forbidden_sudo, incorrect_header, incorrect_body]);
+        )
+        .any_r_for_any_m();
+        let validator_bash = TemplateValidator::new(vec![error_bash_file]);
         if let Err(errors) = validator_bash.async_validate(bash_script).await {
             for error in errors {
                 println!("{}", error);
