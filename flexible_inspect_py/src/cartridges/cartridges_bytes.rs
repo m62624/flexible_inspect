cfbac034c919a141df9eec94a9f6640a0236a3ad
Mansur Azatbek <mansur62624@gmail.com>
2023-08-23 20:46:17 +0600

    docs: updated documentation

--- flexible_inspect_py/src/cartridges/cartridges_bytes.rs
+++ flexible_inspect_py/src/cartridges/cartridges_bytes.rs
@@ -38,6 +38,18 @@ impl TryFrom<PyCartridgeBytes> for Cartridge<RuleBytes> {
     }
 }
 
+// In the validator, we always put the rules into cartridges and the cartridges themselves into template_validator.
+// This means that after applying modifiers, we need to get the same structure, but with different data.
+// But when exporting to other languages, there is no ownership check when using `self`. But most likely there is a check with `&mut self`.
+// To make changes safe, we use `std::mem::take`.
+// This approach allows us to temporarily take data from an object without compromising its integrity.
+// We then return the modified data back to the object.
+// Yes, if you double `std::mem::take` you will get `None`, but this way you can safely call `panic!`,
+// with your own warning why it happened and what to do about it
+// If you export to other languages, don't worry,
+// this is simply a way to safely change the state of objects passed to the &mut self method.
+// This ensures efficient data management and predictable behavior when working
+// with the library in different programming languages.
 impl TryFrom<&mut PyCartridgeBytes> for PyCartridgeBytes {
     type Error = PyErr;
 
