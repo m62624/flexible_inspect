889c57cfa3923425fec3e50545c8702b8bca21e9
Mansur Azatbek <mansur62624@gmail.com>
2023-08-25 14:08:25 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/rules/traits.rs
+++ flexible_inspect_rs/src/rules/traits.rs
@@ -49,24 +49,9 @@ pub trait RuleModifiers {
     type RuleType;
 
     /// modifier for extending the rule with nested rules
-    ///
-    /// ( **by default, all rules must pass every match check** )\
-    /// In this mode, to which all additional rules apply (default mode for everyone).
-    /// We check that for each match (text) all the rules will work.
-    /// ## Operation scheme of the mode
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///      |   [123], [456], [789]
-    ///      |___ Subrule ".+" (MustBeFound) ---> [123] -> [456] -> [789] -- TRUE
-    ///      |                                      |       |        |
-    ///      |___ Subrule "\[\d+\]" (MustBeFound) __|_______|________|
-    ///
-    /// ```
+
+    /// ( **by default, `all_rules_for_all_matches`** )\
+    /// In this mode, all rules must be tested for all matches
     fn extend<R: IntoIterator<Item = Self::RuleType>>(self, nested_rules: R) -> Self::RuleType;
     /// modifier to set the match counter, condition `counter == match`
     fn counter_is_equal(self, count: usize) -> Self::RuleType;
@@ -76,54 +61,14 @@ pub trait RuleModifiers {
     fn counter_less_than(self, count: usize) -> Self::RuleType;
     /// modifier to change the rule matching mode.
     ///
-    /// In this mode, `all the sub-rule` should work for at least `one match`.
-    /// If at least one sub-rule does not work on one of the matches, an error will be returned.
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError (Cartridge)
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///     |   [123], [456], [789]
-    ///     |___ Subrule ".+" (MustBeFound) ---> [123] -- TRUE
-    ///     |                                      |
-    ///     |___ Subrule "\[\d+\]" (MustBeFound) __|
-    ///     |___ Subrule "[a-z]+" (MustBeFound) ---> No Match -- ERROR
-    /// ```
+    /// In this mode, all rules must pass the test for at least one match
     fn all_r_for_any_m(self) -> Self::RuleType;
     /// modifier to change the rule matching mode.
     ///
-    /// In this mode, at least `one sub-rule` should work for `every match`. If no sub-rule works on one of the matches, an error will be returned.
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError (Cartridge)
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///     |   [123], [456], [789]
-    ///     |___ Subrule ".+" (MustBeFound) ---> [123] -- TRUE -- [456] -- TRUE -- [789] -- TRUE
-    ///     |                                      |               |                 |
-    ///     |___ Subrule "\[\d+\]" (MustBeFound) __|_______________|_________________|
-    ///     |___ Subrule "[a-z]+" (MustBeFound) ---> No Match -- TRUE (since other rules matched)
-    /// ```
+    /// In this mode, at least one rule must pass the test for all matches.
     fn any_r_for_all_m(self) -> Self::RuleType;
     /// modifier to change the rule matching mode.
     ///
-    /// In this mode, at least `one sub-rule` should work for at least `one match`. If no sub-rule works on one of the matches, an error will be returned.
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError (Cartridge)
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///     |   [123], [456], [789]
-    ///     |___ Subrule ".+" (MustBeFound) ---> [123] -- TRUE
-    ///     |                                      |
-    ///     |___ Subrule "\[\d+\]" (MustBeFound) __|
-    ///     |___ Subrule "[a-z]+" (MustBeFound) ---> No Match -- TRUE (since other rules matched for at least one match)
-    /// ```
+    /// In this mode, at least one rule must pass at least one match check
     fn any_r_for_any_m(self) -> Self::RuleType;
 }
