756e61fa86ca811c15ab41997a3ec46b95be2447
Mansur Azatbek <mansur62624@gmail.com>
2023-08-25 14:28:58 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/cartridges/traits.rs
+++ flexible_inspect_rs/src/cartridges/traits.rs
@@ -18,21 +18,8 @@ pub trait CartridgeModifiers {
     /// The type of the cartridge that will be returned after applying the modifier
     type CartridgeType;
 
-    /// modifier to change the root rule matching mode,
+    /// modifier to change the rule matching mode.
     ///
-    /// In this mode, at least one sub-rule should work for at least one match. If no sub-rule works on one of the matches, an error will be returned.
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError (Cartridge)
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///     |   [123], [456], [789]
-    ///     |___ Subrule ".+" (MustBeFound) ---> [123] -- TRUE
-    ///     |                                      |
-    ///     |___ Subrule "\[\d+\]" (MustBeFound) __|
-    ///     |___ Subrule "[a-z]+" (MustBeFound) ---> No Match -- TRUE (since other rules matched for at least one match)
-    /// ```
+    /// In this mode, at least one rule must pass at least one match check
     fn any_r_for_any_m(self) -> Self::CartridgeType;
 }
--- flexible_inspect_rs/src/rules/rule_bytes/init.rs
+++ flexible_inspect_rs/src/rules/rule_bytes/init.rs
@@ -7,6 +7,7 @@ impl RuleBytes {
     /// Constructor for creating `RuleBytes`
     ///
     /// # Notes
+    /// * By default, `all_rules_for_all_matches`
     /// * Please stick to *raw string literals* when creating regular expressions, without it your regular expression may behave differently
     /// ## Example
     /// > * `r"d{3}."` - is the correct conversion to a regular expression
@@ -16,23 +17,6 @@ impl RuleBytes {
     /// let rule1 = RuleBytes::new(r"\d+", MatchRequirement::MustBeFound);
     /// let rule2 = RuleBytes::new(r"\s+", MatchRequirement::MustNotBeFound);
     /// ```
-    /// * **By default, all rules must pass every match check**
-    /// In this mode, to which all additional rules apply (default mode for everyone).
-    /// We check that for each match (text) all the rules will work.
-    /// ## Operation scheme of the mode
-    /// ```bash
-    /// #=======================================
-    /// text = "txt [123] txt [456] txt [789]"
-    /// #=======================================
-    /// CustomError
-    /// |
-    /// |__ Rule "\[[^\[\]]+\]" (MustBeFound)
-    ///      |   [123], [456], [789]
-    ///      |___ Subrule ".+" (MustBeFound) ---> [123] -> [456] -> [789] -- TRUE
-    ///      |                                      |       |        |
-    ///      |___ Subrule "\[\d+\]" (MustBeFound) __|_______|________|
-    /// ```
-    ///
     pub fn new<T: Into<String>>(pattern: T, requirement: MatchRequirement) -> Self {
         #[cfg(feature = "log_rust")]
         {
