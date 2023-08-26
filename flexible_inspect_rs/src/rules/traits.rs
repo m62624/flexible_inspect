4257531b6b088ce7af431d06d4155a3d3d1bc7df
Mansur Azatbek <mansur62624@gmail.com>
2023-08-26 20:06:52 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/rules/traits.rs
+++ flexible_inspect_rs/src/rules/traits.rs
@@ -73,6 +73,14 @@ pub trait RuleModifiers {
     /// In this mode, at least one rule must pass at least one match check
     fn any_r_for_any_m(self) -> Self::RuleType;
 
+    /// Before using, create a rule with a regular expression that accepts a number, example of regular expressions for integers and fractions.
+    /// - `\d+`, `\b\d+\b`.
+    /// - `[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?`\
+    /// after, the method automatically converts the received data into a number (all numbers can be of different ranges and data types),
+    /// Supported data types : `i32`, `i64`, `i128`, `f32`, `f64`
+    ///
+    /// # Notes
+    /// Each signed variant can store numbers from `-(2^n - 1) to 2^(n - 1) - 1` inclusive, where n is the number of bits that variant uses. So an `i32` can store numbers from `-(2^31)` to `2^31 - 1`, which equals `-2147483648` to `2147483647`.
     fn number_range<T: PartialOrd>(self, range: std::ops::RangeInclusive<T>) -> Self::RuleType;
 }
 
