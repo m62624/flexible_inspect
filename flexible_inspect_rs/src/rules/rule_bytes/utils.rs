ff15595728771c16e21bba91c42f0468067d8993
Mansur Azatbek <mansur62624@gmail.com>
2023-08-29 13:25:29 +0600

    docs: updated documentation

--- flexible_inspect_rs/src/rules/rule_bytes/utils.rs
+++ flexible_inspect_rs/src/rules/rule_bytes/utils.rs
@@ -15,6 +15,14 @@ impl CalculateValueRules<'_, &[u8]> for RuleBytes {
 }
 
 impl RuleBytes {
+    /// Before using, create a rule with a regular expression that accepts a number, example of regular expressions for integers and fractions.
+    /// - `\d+`, `\b\d+\b`.
+    /// - `[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?`\
+    /// after, the method automatically converts the received data into a number (all numbers can be of different ranges and data types).
+    /// - Supported data types : `i8`, `i16`, `i32`, `i64`, `i128`, `f32`, `f64`
+    ///
+    /// # Notes
+    /// Each signed variant can store numbers from `-(2^n - 1) to 2^(n - 1) - 1` inclusive, where n is the number of bits that variant uses. So an `i32` can store numbers from `-(2^31)` to `2^31 - 1`, which equals `-2147483648` to `2147483647`.
     pub fn number_range<RNG: RangeType>(
         mut self,
         range: RNG,
