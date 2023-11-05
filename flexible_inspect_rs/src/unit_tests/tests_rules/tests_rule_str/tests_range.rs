a75af210238ab7ea0a7946bd448a7b3e89f41392
Mansur Azatbek <mansur62624@gmail.com>
2023-11-05 15:42:41 +0600

    test: added unit tests

--- flexible_inspect_rs/src/unit_tests/tests_rules/tests_rule_str/tests_range.rs
+++ flexible_inspect_rs/src/unit_tests/tests_rules/tests_rule_str/tests_range.rs
@@ -40,13 +40,42 @@ pub fn test_number_range_t_2() {
 
 #[test]
 pub fn test_number_range_t_3() {
-    let text = "1 2 3";
+    let text = "1 1 4 5 6 7 8";
     let validator_numbers = TemplateValidator::new([Cartridge::new(
         0,
         "error message from cartridge 1",
         [Rule::new(r"\d+", MatchRequirement::MustBeFound)
-            .number_range(1..=4, RangeMode::Exactly(100))],
+            .save_duplicates()
+            .number_range(1..=4, RangeMode::Exactly(2))],
     )]);
 
-    dbg!(validator_numbers.validate(text));
+    assert!(validator_numbers.validate(text).is_err());
+}
+
+#[test]
+pub fn test_number_range_t_4() {
+    let text = "1 1 4 7 8";
+    let validator_numbers = TemplateValidator::new([Cartridge::new(
+        0,
+        "error message from cartridge 1",
+        [Rule::new(r"\d+", MatchRequirement::MustBeFound)
+            .save_duplicates()
+            .number_range(5..=7, RangeMode::Any)],
+    )]);
+
+    assert!(validator_numbers.validate(text).is_ok());
+}
+
+#[test]
+pub fn test_number_range_t_5() {
+    let text = "1 1 4 7 8";
+    let validator_numbers = TemplateValidator::new([Cartridge::new(
+        0,
+        "error message from cartridge 1",
+        [Rule::new(r"\d+", MatchRequirement::MustBeFound)
+            .save_duplicates()
+            .number_range(10..=11, RangeMode::Any)],
+    )]);
+
+    assert!(validator_numbers.validate(text).is_err());
 }
